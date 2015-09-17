43d642b468cee5924b0336e7652a4f50b438dd52
Jen Daniels <jdaniels@odu.edu>
2015-09-17 18:19:49 -0400

    Pulled items out and write them with functions. Created dummy boxes.

--- style/testModuleList.php
+++ style/testModuleList.php
@@ -1,34 +1,76 @@
-<!doctype html>
+<?PHP include_once 'iconVariables.php' ?>
+<?PHP include_once 'mockupFunctions.php' ?>
 <?PHP 
 	$css = (isset($_GET['css'])? $_GET['css']: "oduColors.css");	
 	$page = (isset($_GET['iFramePage'])? $_GET['iFramePage']: "testTags.php");	
+
+	$icons = array (
+		"prev" 			=> $iconNavPrevious,
+		"next" 			=> $iconNavNext,
+		"menu" 			=> $iconHamburgerMenu,
+		"notification" 	=> $iconNotification,
+		"user" 			=> $iconUser			
+	);
+
+	$nav = ""; 
+	$breadCrumbs = "";
+	
+	$boxes = array (
+		array(
+			"title" => "1. Overview",
+			"isCollapsed" => 1,
+			"isComplete" => 1,
+			"urlInProgress" => ""),
+		array(
+			"title" => "2. Fundamental Principles",
+			"isCollapsed" => 1,
+			"isComplete" => 0,
+			"urlInProgress" => ""),
+		array(
+			"title" => "3. Bending of Beams",
+			"isCollapsed" => 1,
+			"isComplete" => 0,
+			"urlInProgress" => "something.php"),
+		array(
+			"title" => "4. Shearing Stress",
+			"isCollapsed" => 1,
+			"isComplete" => 1,
+			"urlInProgress" => ""),
+		array(
+			"title" => "5. Stress in Any Given Direction",
+			"isCollapsed" => 1,
+			"isComplete" => 0,
+			"urlInProgress" => ""),
+		array(
+			"title" => "6. Design for Cyclic Loading",
+			"isCollapsed" => 1,
+			"isComplete" => 1,
+			"urlInProgress" => ""),
+		array(
+			"title" => "7. Design of Shafts",
+			"isCollapsed" => 1,
+			"isComplete" => 0,
+			"urlInProgress" => "")
+		);
+			
 ?>
-<?PHP include 'iconVariables.php' ?>
+
+<!doctype html>
 <html>
 	<head>
-		<title>Test</title>
-
-		<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.4.0/css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="<?PHP echo $css ?>">
-	
-		<!--fonts from Adobe Typekit -->
-		<script src="https://use.typekit.net/scm3ciw.js"></script>
-		<script>try{Typekit.load({ async: true });}catch(e){}</script>
-
+		<?php writeHead('Test Text Content', $css); ?>
 	</head>
 	
 	<body>
-	
-		<header>
-			<?php echo $iconHamburgerMenu ?>
-			<div class="oduIcon">ODU ICON</div>
-			<h1 class="courseTitle">MET 320 - Design of Machine Elements</h1>
-			search
-			<?php echo $iconNotification ?>
-			<?php echo $iconUser ?>
-		</header>
+		<?php writeTop($icons, $nav, 'MET 320 - Design of Machine Elements', $breadCrumbs); ?>
+		<?php writeCourseInfoMenu() ?>
 
+		<div class="contentWrapper">
+			<?php 
+				for ($i=0; $i<count($boxes); $i++)
+					writeBox($boxes[$i]["title"], $boxes[$i]["isCollapsed"], $boxes[$i]["isComplete"]);
+			?>
+		</div>
 		
 	</body>
 </html>
-
