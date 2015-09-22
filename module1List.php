46af2b5da05b4188f0f0fe147412b6e3c9fdb762
Jen Daniels <jdaniels@odu.edu>
2015-09-22 15:49:01 -0400

    pulled module 1 list out into its own file for readability

---
+++ module1List.php
@@ -0,0 +1,60 @@
+
+
+<ul class="moduleContentList">
+	<li><i class="fa fa-check-circle success"></i><a href="#">1.1 Overview</a></li>
+	<li><i class="fa fa-check-circle success"></i><a href="#"><i class='fa fa-calendar'></i> 1.A Complete Reading </a><time class="disabled">due 1/14/2015 at 11:59 PM</time></li>
+	<li><i class="fa fa-check-circle success"></i><a href="#">1.2 Course Introduction and Statistical Equilbrium</a>
+		<ul>
+			<li><i class="fa fa-check-circle success"></i><a href="testTextContent.php?css=<?php echo $css ?>">1.2.1 Introduction to Design of Machine Elements [WORKING]</a></li>
+			<li><i class="fa fa-check-circle success"></i><a href="testVideoContent.php?css=<?php echo $css ?>">1.2.2 Defining Engineering and the Design Process [WORKING]</a></li>
+			<li><i class="fa fa-check-circle success"></i><a href="#">1.2.3 Stages of Design</a></li>
+			<li><i class="fa fa-check-circle success"></i><a href="#">1.2.4 Utilizing Machine Design Information and Standards</a></li>
+			<li><i class="fa fa-check-circle success"></i><a href="#">1.2.5 Computational Tools</a></li>
+			<li><i class="fa fa-check-circle success"></i><a href="#">1.2.6 Defining Statistical Equilbrium</a></li>
+	</ul>
+	<li><a href="#"><i class='fa fa-calendar urgent'></i> 1.B Homework #1  </a><time class="urgent">due 1/18/2015 at 11:59 PM</time></li>
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.3 Engineering Materials</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.3.1 Assumptions</a></li>
+		</ul>
+	</li>
+
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.4 Comprehensive Stress and Strain</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.4.1 Tension and Compression Strain</a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.4.2 Strain</a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">Stress and Strain Diagram</a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">Hooke's Law</a></li>	
+		</ul>
+	</li>
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.5 Tension and SI Units</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.5.1 Conversion Factors</li>
+		</ul>
+	</li>
+	
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#"><i class='fa fa-calendar urgent'></i> 1.C Greeting Message  </a><time class="urgent">due 1/18/2015 at 11:59 PM</time></li>
+	
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.6 Force and Mass</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.6.1 Force and Mass</a></li>	
+		</ul>
+	</li>
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.7 Statistically Indeterminate Problems</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.7.1 Statistically Indeterminate Problems</a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.7.2 Example Problem 1</a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.7.3 Example Problem 2</a></li>
+		</ul>
+	</li>
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.8 Center of Gravity</a>
+		<ul>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.8.1 Center of Gravity </a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.8.2 Composite Areas </a></li>
+			<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.8.3 Example Problem 3</a></li>
+		</ul>
+	</li>
+
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#">1.9 Summary</a></li>
+	<li><i class="fa fa-check-circle success hidden"></i><a href="#"><i class='fa fa-calendar urgent'></i> 1.D Module Feedback </a><time class="urgent">due 1/19/2015 at 11:59 PM</time></li>
+</ul>
