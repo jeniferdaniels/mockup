a8b0d439070ded945ee2e87e59918b85832cdbe8
Jen Daniels <jdaniels@odu.edu>
2015-10-01 16:59:16 -0400

    started using global icon variables again

---
+++ iconVariables.php
@@ -0,0 +1,27 @@
+<?php
+
+$listCheckMark = '<i class="fa fa-check-circle success fa-lg"></i>';
+$listCheckMarkHidden = '<i class="fa fa-check-circle success fa-lg hidden"></i>';
+$listAssignment = '<i class="fa fa-calendar fa-lg"></i>';
+$listAssignmentUrgent = '<i class="fa fa-calendar fa-lg urgent"></i>';
+$listCurrentSpot = '<i class="fa fa-arrow-right fa-lg current"></i>';
+
+$assignmentCheckMarkUnchecked = '<i class="fa fa-check-circle disabled fa-3x"></i>';
+$assignmentCheckMarkChecked = '<i class="fa fa-check-circle success fa-3x"></i>';
+
+$iconFileTypePdfSmall = "<i class='fa fa-file-pdf-o'></i>";
+$iconFileTypePdfMedium = "<i class='fa fa-file-pdf-o fa-lg'></i>";
+$iconFileTypeAudioSmall = "<i class='fa fa-file-sound-o'></i>";
+$iconFileTypeAudioMedium = "<i class='fa fa-file-sound-o fa-lg'></i>";
+$iconFileTypeVideoSmall = "<i class='fa fa-file-video-o'></i>";
+$iconFileTypeVideoMedium = "<i class='fa fa-file-video-o fa-lg'></i>";
+$iconFileTypeWordSmall = "<i class='fa fa-file-word-o'></i>";
+$iconFileTypeWordMedium = "<i class='fa fa-file-word-o fa-lg'></i>";
+$iconFileTypePowerpointSmall = "<i class='fa fa-file-powerpoint-o'></i>";
+$iconFileTypePowerpointMedium = "<i class='fa fa-file-powerpoint-o fa-lg'></i>";
+$iconFileTypeExcelSmall = "<i class='fa fa-file-excel-o'></i>";
+$iconFileTypeExcelMedium = "<i class='fa fa-file-excel-o fa-lg'></i>";
+$iconLinkSmall = "<i class='fa fa-link'></i>";
+$iconLinkMedium = "<i class='fa fa-link fa-lg'></i>";
+
+?>
