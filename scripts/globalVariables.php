a4da9305ede55593990bc285d2b6623662805ff2
Jen Daniels <jdaniels@odu.edu>
2015-10-08 17:42:36 -0400

    button icons changed all icons to fixed width

--- scripts/globalVariables.php
+++ scripts/globalVariables.php
@@ -1,32 +1,36 @@
 <?php
 
-$listCheckMark = '<i class="fa fa-check-circle success fa-lg"></i>';
-$listCheckMarkHidden = '<i class="fa fa-check-circle success fa-lg hidden"></i>';
-$listAssignment = '<i class="fa fa-calendar fa-lg"></i>';
-$listAssignmentUrgent = '<i class="fa fa-calendar fa-lg urgent"></i>';
+$listCheckMark = '<i class="fa fa-check-circle success fa-lg fa-fw"></i>';
+$listCheckMarkHidden = '<i class="fa fa-check-circle success fa-lg fa-fw hidden"></i>';
+$listAssignment = '<i class="fa fa-calendar fa-lg fa-fw"></i>';
+$listAssignmentUrgent = '<i class="fa fa-calendar fa-lg fa-fw urgent"></i>';
 
-$listCurrentSpotSmall = '<i class="fa fa-arrow-right current"></i>';
-$listCurrentSpotMedium = '<i class="fa fa-arrow-right fa-lg current"></i>';
-$listCurrentSpotLarge = '<i class="fa fa-arrow-right fa-2x current"></i>';
+$listCurrentSpotSmall = '<i class="fa fa-arrow-right fa-fw current"></i>';
+$listCurrentSpotMedium = '<i class="fa fa-arrow-right fa-lg fa-fw current"></i>';
+$listCurrentSpotLarge = '<i class="fa fa-arrow-right fa-2x fa-fw current"></i>';
 
-$assignmentCheckMarkUnchecked = '<i class="fa fa-check-circle disabled fa-3x"></i>';
-$assignmentCheckMarkChecked = '<i class="fa fa-check-circle success fa-3x"></i>';
+$assignmentCheckMarkUnchecked = '<i class="fa fa-check-circle disabled fa-fw fa-3x"></i>';
+$assignmentCheckMarkChecked = '<i class="fa fa-check-circle success fa-3x fa-fw"></i>';
 
-$iconFileTypePdfSmall = "<i class='fa fa-file-pdf-o'></i>";
-$iconFileTypePdfMedium = "<i class='fa fa-file-pdf-o fa-lg'></i>";
-$iconFileTypeAudioSmall = "<i class='fa fa-file-sound-o'></i>";
-$iconFileTypeAudioMedium = "<i class='fa fa-file-sound-o fa-lg'></i>";
-$iconFileTypeVideoSmall = "<i class='fa fa-file-video-o'></i>";
-$iconFileTypeVideoMedium = "<i class='fa fa-file-video-o fa-lg'></i>";
-$iconFileTypeWordSmall = "<i class='fa fa-file-word-o'></i>";
-$iconFileTypeWordMedium = "<i class='fa fa-file-word-o fa-lg'></i>";
-$iconFileTypePowerpointSmall = "<i class='fa fa-file-powerpoint-o'></i>";
-$iconFileTypePowerpointMedium = "<i class='fa fa-file-powerpoint-o fa-lg'></i>";
-$iconFileTypeExcelSmall = "<i class='fa fa-file-excel-o'></i>";
-$iconFileTypeExcelMedium = "<i class='fa fa-file-excel-o fa-lg'></i>";
-$iconLinkSmall = "<i class='fa fa-link'></i>";
-$iconLinkMedium = "<i class='fa fa-link fa-lg'></i>";
+$iconFileTypePdfSmall = '<i class="fa fa-file-pdf-o fa-fw"></i>';
+$iconFileTypePdfMedium = '<i class="fa fa-file-pdf-o fa-lg fa-fw"></i>';
+$iconFileTypeAudioSmall = '<i class="fa fa-file-sound-o fa-fw"></i>';
+$iconFileTypeAudioMedium = '<i class="fa fa-file-sound-o fa-lg fa-fw"></i>';
+$iconFileTypeVideoSmall = '<i class="fa fa-file-video-o fa-fw"></i>';
+$iconFileTypeVideoMedium = '<i class="fa fa-file-video-o fa-lg fa-fw"></i>';
+$iconFileTypeWordSmall = '<i class="fa fa-file-word-o fa-fw"></i>';
+$iconFileTypeWordMedium = '<i class="fa fa-file-word-o fa-lg fa-fw"></i>';
+$iconFileTypePowerpointSmall = '<i class="fa fa-file-powerpoint-o fa-fw"></i>';
+$iconFileTypePowerpointMedium = '<i class="fa fa-file-powerpoint-o fa-lg fa-fw"></i>';
+$iconFileTypeExcelSmall = '<i class="fa fa-file-excel-o fa-fw"></i>';
+$iconFileTypeExcelMedium = '<i class="fa fa-file-excel-o fa-lg fa-fw"></i>';
+$iconLinkSmall = '<i class="fa fa-link fa-fw"></i>';
+$iconLinkMedium = '<i class="fa fa-link fa-lg fa-fw"></i>';
 
-
-
+$iconSearchSmall = '<i class="fa fa-search fa-fw"></i>';
+$iconSearchMedium = '<i class="fa fa-search fa-lg fa-fw"></i>';
+$iconUserSmall = '<i class="fa fa-user fa-fw"></i>';
+$iconUserMedium = '<i class="fa fa-user fa-lg fa-fw"></i>';
+$iconChatSmall = '<i class="fa fa-comments fa-fw"></i>';
+$iconChatMedium = '<i class="fa fa-comments fa-lg fa-fw"></i>';
 ?>
