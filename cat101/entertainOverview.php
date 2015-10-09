f2460f028796c26ad3bf09e6cb6f3a2a0466ffde
Jen Daniels <jdaniels@odu.edu>
2015-10-09 16:33:32 -0400

    added dummy content

--- cat101/entertainOverview.php
+++ cat101/entertainOverview.php
@@ -1,4 +1,34 @@
+<?php include_once '../scripts/mockupFunctions.php' ?>
+<?php include_once '../scripts/globalVariables.php' ?>
 <?php 
-echo __FILE__;
+	$navPrevious = "";
+	$navNext = "";
+	$showModuleProgress = 1;
+	$pageTitle = "Content Goes Here";
+	
+	$breadCrumbs = array();
 ?>
-<iframe width="854" height="480" src="https://www.youtube.com/embed/8zduX80-iGM" frameborder="0" allowfullscreen></iframe>
+
+<!doctype html>
+<html>
+	<head>
+		<?php writeHead(getCourseName()); ?>
+	</head>
+	
+	<body>
+		<?php writeTop($navNext, $navPrevious, $showModuleProgress, $breadCrumbs); ?>
+	
+		<div class="contentWrapper" id="contentWrapper">
+			
+			<h2><?php echo $pageTitle ?></h2>
+				<?php echo __FILE__?>
+				<iframe width="854" height="480" src="https://www.youtube.com/embed/8zduX80-iGM" frameborder="0" allowfullscreen></iframe>
+			   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.
+			   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.
+			   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
+			</div>
+				
+		<footer><?php writeFooter() ?></footer>
+		
+	</body>
+</html>
