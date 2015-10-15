1ad4a20274a3a3146daff4719311803c6927a9cd
Jen Daniels <jdaniels@odu.edu>
2015-10-15 16:19:21 -0400

    moved calendar js out into mockup functions and into calendarDemo.js updated main div to content wrapper with dash modifier. changed scheduleContent to scheduleContentWrapper

--- cat101/index.php
+++ cat101/index.php
@@ -1,5 +1,6 @@
 <?php include_once '../scripts/mockupFunctions.php' ?>
 <?php include_once '../scripts/globalVariables.php' ?>
+
 <?php 
 	$isExpandedList = (isset($_GET['isExpanded'])? $_GET['isExpanded']: "0,1,0,0");
 	$isDoneList = (isset($_GET['isDone'])? $_GET['isDone']: "1,0,0,0");
@@ -55,127 +56,22 @@
 <!doctype html>
 <html>
 	<head>
-		<?php writeHead(getCourseName()); ?>
-		<link href='../calendar/fullcalendar.css' rel='stylesheet' />
-		<link href='../calendar/fullcalendar.print.css' rel='stylesheet' media='print' />
-		<script src='../calendar/lib/moment.min.js'></script>
-		<script src='../calendar/lib/jquery.min.js'></script>
-		<script src='../calendar/fullcalendar.min.js'></script>
-		<script>
+		<?php writeCourseDashboardHead(""); ?>
 
-			$(document).ready(function() {
-				
-				$('#calendar').fullCalendar({
-					header: {
-						left: 'prev,next',
-						center: 'title',
-						right: 'month,basicWeek,agendaText'
-					},
-					views:{
-						agendaText:{
-							type: 'agenda',
-							buttonText: 'text'
-							}
-						},
-					defaultDate: '2015-01-18',
-					editable: false,
-					eventLimit: true, // allow "more" link when too many events
-					events: [
-						{
-							title: 'Overview and Course Logistics',
-							start: '2015-01-10',
-							end: '2015-01-15',
-							color: "#EEEEEE",
-							textColor: "black"
-						},
-						{
-							title: 'Choosing a Kitten',
-							start: '2015-01-15',
-							end: '2015-01-23',
-							color: "#EEEEEE",
-							textColor: "black"
-						},
-						{
-							title: 'Caring for Your Kitten',
-							start: '2015-01-23',
-							end: '2015-01-30',
-							color: "#EEEEEE",
-							textColor: "black"
-						},
-						{
-							title: 'Legal Requiremens of Owning Kittens',
-							start: '2015-01-30',
-							end: '2015-02-07',
-							color: "#EEEEEE",
-							textColor: "black"
-						},
-						{
-							title: '0.A - Send Test Email',
-							start: '2015-01-12T23:59:00'
-						},
-						{
-							title: '0.B - Module Feedback',
-							start: '2015-01-15T23:59:00'
-						},
-						{
-							title: '1.A - Homework #1',
-							start: '2015-01-18T23:59:00'
-						},
-						{
-							title: '1.B Discussion Forum #1',
-							start: '2015-01-20T23:59:00'
-						},
-						{
-							title: '1.C Quiz',
-							start: '2015-01-22T23:59:00'
-						},
-						{
-							title: '1.D Module Feedback',
-							start: '2015-01-22T23:59:00'
-						},
-						{
-							title: '2.A Homework #2',
-							start: '2015-01-25T23:59:00'
-						},
-						{
-							title: '2.B Discussion Forum #2',
-							start: '2015-01-27T23:59:00'
-						},
-						{
-							title: '2.C Homework #3',
-							start: '2015-01-29T23:59:00'
-						},
-						{
-							title: '2.D Module Feedback',
-							start: '2015-01-30T23:59:00'
-						},
-						{
-							title: '3.A Homework #4',
-							start: '2015-02-02T23:59:00'
-						},
-						{
-							title: '3.B Discussion Forum #3',
-							start: '2015-02-05T23:59:00'
-						},
-						{
-							title: '3.C Final Exam',
-							start: '2015-02-06T23:59:00'
-						},
-						{
-							title: '3.D Module Feedback',
-							start: '2015-02-07T23:59:00'
-						}
-					]
-				});
-				
+		
+		<script>
+			
+			$(document).ready(function(){
+				recurringCalendar();
+				//overwrite the stupid thing.
+				document.getElementById("oduCal").style ="height: 355px";
 			});
 		</script>
 	</head>
 	
 	<body>
-	<?php writeTop($navNext, $navPrevious, $showModuleProgress, ""); ?>
-		<div class="indexContent">
-		
+		<?php writeTop($navNext, $navPrevious, $showModuleProgress, ""); ?>
+		<div class="contentWrapper dash">		
 			<?php 
 				for ($i=0; $i<count($boxes); $i++)
 					writeSuccessMessage($i, "You have successfully completed module " . $i . "."); 
@@ -185,18 +81,17 @@
 			?>
 			
 		
-			<div class="scheduleContent">
+			<div class="scheduleContentWrapper">
+				<?php writeDashWidgetTitle("Schedule At a Glance", true); ?>
 				<div id='calendar'></div>
 			</div>
 			
-			<div class="courseContent">
-			<?php 
-				for ($i=0; $i<count($boxes); $i++)
-					writeToggleBox($boxes[$i]);
-			?>
-			</div>
-			
-			
+			<div class="courseContentWrapper">
+				<?php writeDashWidgetTitle("Course Content", false); ?>
+				<?php 
+					for ($i=0; $i<count($boxes); $i++)
+						writeToggleBox($boxes[$i]);
+				?>
 			</div>
 		
 		</div>
