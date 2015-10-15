8782127ad70b8c4ff5e690a979fb4cd108ddee8c
Jen Daniels <jdaniels@odu.edu>
2015-10-15 16:00:33 -0400

    removed control functionality.

--- aaq.php
+++ aaq.php
@@ -1,103 +1,90 @@
-<?php include_once 'scripts/mockupFunctions.php' ?>
-<?php include_once 'scripts/globalVariables.php' ?>
-
-<!doctype html>
-<html>
-	<head>
-		<?php writeHead(getCourseName()); ?>
-	</head>
+<div class="aaq">
+	<!--
+	<div>
+		<div class="displayInlineBlock"><?php echo $GLOBALS["iconAaqLarge"]; ?></div>
+		<h2 class="displayInlineBlock">Questions</h2>
+		<nav>
+		<ul class="aaqHeaderLinks floatLeft">
+			<li><a href="">Ask A New Question</a></li>
+		</ul>
+		
+		<ul class="aaqHeaderLinks floatRight">
+			<li><a href="">All </a></li>
+			<li><a href="">Pinned</a> </li>
+			<li><a href="">My Posts</a></li>
+		</ul>
+		</nav>
+		
+	</div>
+	-->
 	
-	<body>
-		<div class="aaqContentWrapper">
-		<hr>
-
-			<div>
-				<div class="displayInlineBlock"><?php echo $GLOBALS["iconAaqLarge"]; ?></div>
-				<h2 class="displayInlineBlock">Questions</h2>
-				<nav>
-				<ul class="aaqHeaderLinks floatLeft">
-					<li><a href="">Ask A New Question</a></li>
-				</ul>
-				
-				<ul class="aaqHeaderLinks floatRight">
-					<li><a href="">All </a></li>
-					<li><a href="">Pinned</a> </li>
-					<li><a href="">My Posts</a></li>
-				</ul>
-				</nav>
-				
+	<div class="aaqQuestionAndAnswerWrapper">
+		<div class="aaqQuestion">
+			<span class="aaqPostTitle"><?php echo $GLOBALS["iconExpandedSmall"]; ?>Some question short title here</span>
+			<div class="floatRight">
+				 <?php echo $GLOBALS["iconPinSmall"]; ?>
 			</div>
 			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas</p>
 			
-			<div class="aaqQuestionAndAnswerWrapper">
-				<div class="aaqQuestion">
-					<span class="aaqPostTitle"><?php echo $GLOBALS["iconExpandedSmall"]; ?>Some question short title here</span>
-					<div class="floatRight">
-						 <?php echo $GLOBALS["iconPinSmall"]; ?>
-					</div>
-					
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas</p>
-					
-					<ul class="aaqPostProperties">
-						<li><a href="">Reply</a></li>
-						<li class="aaqPostRating"></li>
-						<li class="aaqPostEndorsed"></li>
-						<li class="aaqPostCommentCount">2 comments</li>
-						<li class="aaqPoster">Darly Dixon</li>
-						<li class="aaqPostDateTime">1/18/2015 10:55 AM</li>
-					</ul>
-					
-				</div>
-				
-				<div class="aaqAnswer">
-					<span class="aaqPostTitle">Some answer short title here</span>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
-					
-					<ul class="aaqPostProperties">
-						<li><a href="">Reply</a></li>
-						<li class="aaqPostRating"><?php echo $GLOBALS["icon3AndAHalfOf5StarsSmall"]; ?></li>
-						<li class="aaqPostEndorsed"></li>
-						<li class="aaqPostCommentCount">0 comments</li>
-						<li class="aaqPoster">Carl Grimes</li>
-						<li class="aaqPostDateTime">1/18/2015 12:15 PM</li>
-					</ul>
-				</div>
+			<ul class="aaqPostProperties">
+				<li><a href="">Reply</a></li>
+				<li class="aaqPostRating"></li>
+				<li class="aaqPostEndorsed"></li>
+				<li class="aaqPostCommentCount">2 comments</li>
+				<li class="aaqPoster">Darly Dixon</li>
+				<li class="aaqPostDateTime">1/18/2015 10:55 AM</li>
+			</ul>
+			
+		</div>
+		
+		<div class="aaqAnswer">
+			<span class="aaqPostTitle">Some answer short title here</span>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
+			
+			<ul class="aaqPostProperties">
+				<li><a href="">Reply</a></li>
+				<li class="aaqPostRating"><?php echo $GLOBALS["icon3AndAHalfOf5StarsSmall"]; ?></li>
+				<li class="aaqPostEndorsed"></li>
+				<li class="aaqPostCommentCount">0 comments</li>
+				<li class="aaqPoster">Carl Grimes</li>
+				<li class="aaqPostDateTime">1/18/2015 12:15 PM</li>
+			</ul>
+		</div>
 
-				<div class="aaqAnswer">
-					<span class="aaqPostTitle">Some answer short title here</span>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
-					
-					<ul class="aaqPostProperties">
-						<li><a href="">Reply</a></li>
-						<li class="aaqPostRating"><?php echo $GLOBALS["icon3AndAHalfOf5StarsSmall"]; ?></li>
-						<li class="aaqPostEndorsed">Instructor Endorsed</li>
-						<li class="aaqPostCommentCount">0 comments</li>
-						<li class="aaqPoster">Beth Greene</li>
-						<li class="aaqPostDateTime">1/18/2015 5:30 AM</li>
-					</ul>
-				</div>
-			</div><!-- end aaqQuestionAndAnswerWrapper -->
+		<div class="aaqAnswer">
+			<span class="aaqPostTitle">Some answer short title here</span>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
+			
+			<ul class="aaqPostProperties">
+				<li><a href="">Reply</a></li>
+				<li class="aaqPostRating"><?php echo $GLOBALS["icon3AndAHalfOf5StarsSmall"]; ?></li>
+				<li class="aaqPostEndorsed">Instructor Endorsed</li>
+				<li class="aaqPostCommentCount">0 comments</li>
+				<li class="aaqPoster">Beth Greene</li>
+				<li class="aaqPostDateTime">1/18/2015 5:30 AM</li>
+			</ul>
+		</div>
+	</div><!-- end aaqQuestionAndAnswerWrapper -->
 
 
-			<div class="aaqQuestionAndAnswerWrapper">
-				<div class="aaqQuestion">
-					<span class="aaqPostTitle"><?php echo $GLOBALS["iconCollapsedSmall"]; ?>Some question short title here</span>
-					<div class="floatRight">
-						 <?php echo $GLOBALS["iconPinSmall"]; ?>
-					</div>
-					
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas</p>
-					
-					<ul class="aaqPostProperties">
-						<li><a href="">Reply</a></li>
-						<li class="aaqPostRating"></li>
-						<li class="aaqPostEndorsed"></li>
-						<li class="aaqPostCommentCount">1 comments</li>
-						<li class="aaqPoster">Maggie Greene</li>
-						<li class="aaqPostDateTime">1/17/2015 10:25 AM</li>
-					</ul>
-				</div>
-			</div><!-- end aaqQuestionAndAnswerWrapper -->
-		</div><!-- end aaqContentWrapper -->		
-	</body>
-</html>
+	<div class="aaqQuestionAndAnswerWrapper">
+		<div class="aaqQuestion">
+			<span class="aaqPostTitle"><?php echo $GLOBALS["iconCollapsedSmall"]; ?>Some question short title here</span>
+			<div class="floatRight">
+				 <?php echo $GLOBALS["iconPinSmall"]; ?>
+			</div>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas</p>
+			
+			<ul class="aaqPostProperties">
+				<li><a href="">Reply</a></li>
+				<li class="aaqPostRating"></li>
+				<li class="aaqPostEndorsed"></li>
+				<li class="aaqPostCommentCount">1 comments</li>
+				<li class="aaqPoster">Maggie Greene</li>
+				<li class="aaqPostDateTime">1/17/2015 10:25 AM</li>
+			</ul>
+		</div>
+	</div><!-- end aaqQuestionAndAnswerWrapper -->
+</div><!-- end aaqContentWrapper -->		
