a3d04b678d220d1ed71418b6d7bcbfc9a4db27dd
Jen Daniels <jdaniels@odu.edu>
2015-10-23 12:16:53 -0400

    fixed checking field inside to put it properly in the loop added get topic, but going to refactor to make a generic getObject function since most of the code is the same.
    added global variable to show console messages.

--- scripts/js/courseFunctions.js
+++ scripts/js/courseFunctions.js
@@ -1,15 +1,24 @@
+//GLOBALS
+var gShowConsoleMsgs = true;
+	
+	
+	
 function populateCourse(url) {
 	$.ajax({	
         url: url
     }).done(function(obj) {
     	writeDocumentTitle(obj);
 		writeInstructorInformation(obj);
-		getProperDisplayOrder(obj.course.modules, "moduleDisplayOrder");
-		writCourseContent(obj);
-    	
-    	
+		
+		myModule = getModuleObject(obj, "moduleDisplayOrder", 3)
+		//stuff = getDisplayOrder(obj.course.modules[1], "moduleDisplayOrder");
+		//writCourseContent(obj);
+
 	});
 };
+
+
+
 
 
 //********************************************************************
@@ -44,72 +53,104 @@ function getModuleObject(obj, field, id){
 	var selectedModule, moduleFieldValue = null; //returning null is better than undefined
 
 	//check to see if there is a module array
-	if (typeof modules != "undefined"){
+	if (typeof modules != "undefined"){	
+		for (var i=0; i< modules.length; i++){
 
-		//check to see if this is even a valid field
-		if (modules[0].hasOwnProperty(field)){
-
-			for (var i=0; i< modules.length; i++){
+			//check to see if this is even a valid field
+			if (modules[i].hasOwnProperty(field)){
 
 				//since we are dynamically getting the field name
 				eval("moduleFieldValue = modules[i]." + field);
 				console.log("module[" + i + "]." + field + " = " + moduleFieldValue)
-				
+					
 				if (moduleFieldValue == id){
-					console.log ("Match for '" + field + "' = '" + id + "' at modules[" + i + "]");
-
+					if (gShowConsoleMsgs) console.log ("Match for '" + field + "' = '" + id + "' at modules[" + i + "]");	
 					selectedModule = modules[i];
 					i=modules.length; //jump out of loop so we can return this properly
-				} else 	console.log("No match for '" + field + "' = '" + id + "' at modules[" + i + "]");
-			}//end loop
-								
-		} else console.log("The first module in the array does not have the property '" + field + "'");
+				} else 	{if (gShowConsoleMsgs) console.log("No match for '" + field + "' = '" + id + "' at modules[" + i + "]");}
 
-	} else console.log("No 'modules' array");
+			} else {if (gShowConsoleMsgs) console.log("modules[" + i +  "] does not have the property '" + field + "'");}
+					
+		}//end loop
+								
+	} else {if (gShowConsoleMsgs) console.log("No 'modules' array");}
 
 	return selectedModule;
 }
 
+
 //*****************************************************************************************
-//Name: 		getIdWithDisplayOrder
-//Input:		obj = json object, specifically the one that needs to get the ordering info
-//
+//Name: 		getTopicObject
+//Input:		obj = json object, the specific module object
+//				field = field name in the topics array
+//				id = some id to look for a match
+//Returns:		topic object if found, null if not found or errors out
+//Description:  looks into the json obj into the topics array under each moduel for 
+//				a field that matches the id.  This is cool because you can 
+//				pass it differnt field names and it will find the module
+//				DO NOT PASS IN "parent" as the field to id on because there can be
+//				several topics in the module and therefore they have the same parent
+//Example Use:	myTopic = getTopicObject(obj.course.modules[1], "title", "Entertaining");
+//				myTopic = getTopicObject(myModule, "moduleDisplayOrder", 1); 
 //*****************************************************************************************
-function getIdAndDisplayOrder(obj, field)
+function getTopicObject(obj, field, id){
+	var topics = obj.topics;
+	var selectedTopic, topicFieldValue = null; //returning null is better than undefined
+
+	//check to see if there is a module array
+	if (typeof topics != "undefined"){
+		
+		for (var i=0; i< topics.length; i++){
+
+				//check to see if this is even a valid field
+				if (topics[i].hasOwnProperty(field)){
+					
+				//since we are dynamically getting the field name
+				eval("topicFieldValue = topics[i]." + field);
+				console.log("topics[" + i + "]." + field + " = " + topicFieldValue)
+				
+				if (moduleFieldValue == id){
+					if (gShowConsoleMsgs) console.log ("Match for '" + field + "' = '" + id + "' at topics[" + i + "]");
+
+					selectedTopic = topics[i];
+					i=topics.length; //jump out of loop so we can return this properly
+				} else 	{if (gShowConsoleMsgs) console.log("No match for '" + field + "' = '" + id + "' at topics[" + i + "]");}
+		
+			} else {if (gShowConsoleMsgs) console.log("topics[" + i +  "] does not have the property '" + field + "'");}
+		}//end loop
+								
+	} else {if (gShowConsoleMsgs) console.log("No 'topics' array");}
+
+	return selectedTopic;
+}
+
+
+//*****************************************************************************************
+//Name: 		getDisplayOrder
+//Input:		obj = json object, depending on what you pass to it it may be the module,
+//				topic, assignment etc
+//				field, depending on the object, the name of the display order field is different
+//				for modules it is "moduleDisplayOrder" for assignments and topics etc it is
+//				"innerModuleDisplayOrder" etc. See JSON file for specifics
+//Returns:		display order int, or null if not found
+//Description:	takes an individual single object, looks for the display order property (passed
+//				in as 'field' because in the different items it can be named something different
+//				returns the integer if found, else returns null
+//				does not take arrays
+//*****************************************************************************************
+function getDisplayOrder(obj, field)
 {
-	var displayOrderValues = [];
-	var itemDisplayOrderValue = null;
+	var itemDisplayOrder = null;
 
 	//check to see if object is legit
 	if (typeof obj != "undefined"){
 
-		if (obj[0].hasOwnProperty(field)){
+		if (obj.hasOwnProperty(field)){
+				eval("itemDisplayOrder = obj." + field);
+		} else {if (gShowConsoleMsgs) console.log("The object does not have the property '" + field + "'");}
+	} else {if (gShowConsoleMsgs) console.log("Object undefined.");}
 
-			for (var i=0; i< obj.length; i++){
-
-				//since we are dynamically getting the field name and the object
-				eval("itemDisplayOrderValue = obj[i]." + field);
-				console.log ("itemDisplayOrderValue = obj[i]." + field);
-
-				displayOrderValues.push(itemDisplayOrderValue);
-				
-			}//end loop
-								
-		} else console.log("The first object in the array does not have the property '" + field + "'");
-
-	} else console.log("Object undefined.");
-
-	//raw data
-	//console.log("Raw data values " + displayOrderValues);
-	
-	//order them 
-	//displayOrderValues.sort(function(a, b){return a-b});
-	//console.log("Items in numerical order " + displayOrderValues);
-	
-	//TODO
-	//add ids to the display order
-	
-	return displayOrderValues;
+	return itemDisplayOrder;
 }
 
 //********************************************************************
