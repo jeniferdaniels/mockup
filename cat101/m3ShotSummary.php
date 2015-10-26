6c5a016900dd3b26d0b58aa20e0ea9b4bf5da7a6
Jen Daniels <jdaniels@odu.edu>
2015-10-26 12:59:09 -0400

    created dummy pages or renamed

---
+++ cat101/m3ShotSummary.php
@@ -0,0 +1,33 @@
+<?php include_once '../scripts/mockupFunctions.php' ?>
+<?php include_once '../scripts/globalVariables.php' ?>
+<?php 
+	$navPrevious = "";
+	$navNext = "";
+	$showModuleProgress = 1;
+	$pageTitle = "Content Goes Here";
+	
+	$breadCrumbs = array();
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<?php writeHead($pageTitle); ?>
+	</head>
+	
+	<body>
+		<?php writeTop($navNext, $navPrevious, $showModuleProgress, $breadCrumbs); ?>
+	
+		<div class="contentWrapper" id="contentWrapper">
+			
+			<h2><?php echo $pageTitle ?></h2>
+				<?php echo __FILE__?>
+			   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.
+			   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.
+			   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>
+			</div>
+				
+		<footer><?php writeFooter() ?></footer>
+		
+	</body>
+</html>
