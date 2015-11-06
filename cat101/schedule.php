c143a8cfd653674fe07a7253ceb95773529b700c
Jen Daniels <jdaniels@odu.edu>
2015-11-06 17:55:37 -0500

    moved the modal to a php function to write out. neatened up style.

--- cat101/schedule.php
+++ cat101/schedule.php
@@ -14,13 +14,19 @@
 		<?php writeHead($pageTitle); ?>
 					
 		<link href='../calendar/fullcalendar.css' rel='stylesheet' />
-		<!-- <link href='../calendar/fullcalendar.print.css' rel='stylesheet' media='print' /> -->		
+		<link href='../calendar/fullcalendar.print.css' rel='stylesheet' media='print' />		
+		<style>
+			.fc-center h2{ font-size: 1.4em; }
+			#eventContent{ z-index: 999; }
+		</style>
+
 
 		<script src='../calendar/lib/moment.min.js'></script>
-		<script src='../calendar/lib/jquery.min.js'></script>
+		<script src='../scripts/js/bootstrapmodal.min.js'></script>	
+		<script src='../calendar/lib/jquery.min.js'></script>		
 		<script src="../scripts/js/jquery-ui.min.js"></script>
 		<script src='../calendar/fullcalendar.min.js'></script>
-		<script src='../scripts/js/bootstrapmodal.min.js'></script>	
+		
 
 		<script>
 		$(document).ready(function(){
@@ -33,7 +39,7 @@
 		    }).done(function(obj) {
 		    	setDocumentTitle(obj);
 				setTop(obj);
-				
+				$('#eventContent').hide();
 				$('#calendar').fullCalendar({
 				    events: getEvents(obj),
 				    height: 750,
@@ -42,13 +48,11 @@
 				        left: 'prev',
 				        center: 'title',
 				        right: 'month,basicWeek,basicDay, next'
-				    },
-				    
+				    },		    
 				    eventRender: function (event, element) {
 				        element.attr('href', 'javascript:void(0);');
 				        element.click(function() {
 
-				            
 					        if (event.type == "assignment"){
 								$("#assignmentDeliverables").html(event.deliverable);
 						        $("#eventDateTime").html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
@@ -67,38 +71,16 @@
 						        $("#assignmentDeliverables").hide();
 						        $("#assignmentCompletionStatus").hide();
 					        }
-
-
 					        $("#eventTitle").html(event.type + " - " + event.title);
 				            $("#eventDescription").html(event.description);
 				            $("#eventContent").dialog({ modal: true, width:500});
 
-					        
 				        });
 				    }
 				});
 			});
 		};
 		</script>
-		
-		
-		
-		<style>
-			.fc-center h2{
-				font-size: 1.4em;
-			}
-		
-			//hide the time in the small version
-			.fc-time{
-				//display: none;
-			}
-			
-
-			#eventContent{
-				z-index: 999;
-			}
-		</style>
-
 	</head>
 	
 	<body>
@@ -109,30 +91,8 @@
 			<div id='calendar'></div>
 		</div>
 		
-		
 		<!-- modal -->
-		<div id="eventContent" title="Event Details" style="display:hidden; z-index: 3000">
-			<div class="modalContentWrapper">
-				<div class="icon" id="eventIcon"><?php echo $GLOBALS['iconAssignmentLarge'] ?></div><h3 id="eventTitle"></h3>
-				<br>
-		    	<h5 id="assignmentDueHeader">Due - </h5>
-		    	<div class="eventDateTime" id="eventDateTime"></div>
-				<br>
-		    	<h5 id="assignmentDeliverableHeader">Deliverable - </h5>
-		    	<div class="assignmentDeliverables" id="assignmentDeliverables"></div>
-
-		    	<p id="eventDescription"></p>
-
-		    	<div id="assignmentCompletionStatus">Assignment not submitted</div>
-		    </div>
-		</div>
-		
-		
-
-
-		
-		
-		
+		<?php writeCalendarModal() ?>
 		
 	</body>
 </html>
