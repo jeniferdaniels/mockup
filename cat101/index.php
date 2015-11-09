553f9fcd79a914bbe91fc22fff0f2aae993c9174
Jen Daniels <jdaniels@odu.edu>
2015-11-09 17:17:04 -0500

    gutted to try to get read more working, but did not work. should refer to earlier version of this file for missing functionality

--- cat101/index.php
+++ cat101/index.php
@@ -7,12 +7,7 @@
 	<head>
 		<?php writeCourseDashboardHead(""); ?>
 		<link href='../calendar/fullcalendar.css' rel='stylesheet' />
-		<style>
-			.fc-center h2{ font-size: 1.4em; }
-			.fc-time {display: none; }
-			.fc-title, .fc-day-number {font-size: .7rem;}
-			#eventContent{ z-index: 999; }
-		</style>
+		<link href='../css/courseDashboard.css' rel='stylesheet'/>
 		
 		
 		<script src='../calendar/lib/moment.min.js'></script>
@@ -22,64 +17,20 @@
 		<script src='../calendar/fullcalendar.min.js'></script>
 		
 		
+		
 		<script>
 		$(document).ready(function(){
-			populateCourse("json/kitten.json");
-			populateSchedule("json/kitten.json");
-
-			var $el, $ps, $up, totalHeight;
-
-			$(".upcomingAssignment .button").click(function() {
-			      
-			  totalHeight = 0
-
-			  $el = $(this);
-			  $p  = $el.parent();
-			  $up = $p.parent();
-			  $ps = $up.find("p:not('.readMore')");
-			  
-			  // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
-			  $ps.each(function() {
-			    totalHeight += $(this).outerHeight();
-			  });
-			        
-			  $up
-			    .css({
-			      // Set height to prevent instant jumpdown when max height is removed
-			      "height": $up.height(),
-			      "max-height": 9999
-			    })
-			    .animate({
-			      "height": totalHeight
-			    });
-			  
-			  // fade out read-more
-			  $p.fadeOut();
-			  
-			  // prevent jump-down
-			  return false;
-			    
-			});
-			
-
+			makeCourseDashboard("json/kitten.json");
 			
 			});
 		
-		function populateCourse(url) {
+		function makeCourseDashboard(url) {
 			$.ajax({	
 		        url: url
 		    }).done(function(obj) {
-		    	setDocumentTitle(obj);
-				setTop(obj);
-				setCourseContent(obj);
-
-			});
-		};
-
-		function populateSchedule(url) {
-			$.ajax({	
-		        url: url
-		    }).done(function(obj) {
+		    	setTop(obj);
+		    	setCourseContent(obj);
+		    	
 				$('#eventContent').hide();
 				$('#calendar').fullCalendar({
 				    events: getEventsAtAGlance(obj),
@@ -90,42 +41,8 @@
 				        left: 'prev',
 				        center: 'title',
 				        right: 'next'
-				    },		    
-				    eventRender: function (event, element) {
-				        element.attr('href', 'javascript:void(0);');
-
-				        if(event.icon){
-				        	element.find(".fc-title").prepend("<i class='fa fa-" + event.icon + "'></a>");
-				        }
-
-				        
-				        element.click(function() {
-
-					        if (event.type == "assignment"){
-								$("#assignmentDeliverables").html(event.deliverable);
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
-								//these will not show if a module was clicked on first, so need to turn these back on
-								$("#eventIcon").show();
-								$("#assignmentDueHeader").show();
-								$("#assignmentDeliverableHeader").show();
-						        $("#assignmentDeliverables").show();
-						        $("#assignmentCompletionStatus").show();
-					        }
-					        else{
-								$("#eventIcon").hide();
-								$("#assignmentDueHeader").hide();
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD') + ' - ' + moment(event.end).format('MMMM DD, YYYY'));
-								$("#assignmentDeliverableHeader").hide();
-						        $("#assignmentDeliverables").hide();
-						        $("#assignmentCompletionStatus").hide();
-					        }
-					        $("#eventTitle").html(event.type + " - " + event.title);
-				            $("#eventDescription").html(event.description);
-				            $("#eventContent").dialog({ modal: true, width:500});
-
-				        });
 				    }
-				});
+			    });
 			});
 		};
 		</script>
@@ -133,50 +50,33 @@
 	</head>
 	
 	<body>
-		<?php writeTop("", "", "", ""); ?>
-		
-		<div class="contentWrapper">		
-			
-		
-			<div class="scheduleContentWrapper">
-				<?php writeDashWidgetTitle("Schedule At a Glance"); ?>
-				<div id='calendar'></div>
-				<a href="schedule.php">Full Schedule</a>
-		
-				<?php writeDashWidgetTitle("Upcoming Assignments")?>
-				
-				<div id="" class="upcomingAssignment">	
-					<div id="" class="upcomingAssignmentDateWrapper">
-						<h2>Jan</h2>
-						<h1>18</h1>
-						<h3>11:59pm</h3>				
-					</div>
-					
-					<h3>Assignment Title Here</h3>
-					<div id="" class="upcomingAssignmentDeliverable">Deliverable Here</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</p>										
-					<p class="readMore"><a href="#" class="button">Read More</a></p>
-				</div>
-					
-				
+		<?php writeTop("", "", "", "") ?>
 	
-				<a href="assignments.php">Full Assignment List</a>
-		
-		
-		
-		
-			</div>
+		<div class="contentWrapper dash" id="dash">
+			<div id="leftHandSide">
+				<div class="scheduleAtAGlance">
+					<h2>Schedule At a Glance</h2>
+					<div id='calendar'></div>
+					<a href="schedule.php">Full Schedule</a>
+				</div>
+				
+				<div class="upcomingAssignments">
+					<h2>Upcoming Assignments</h2>
+						<?php for ($i=0; $i<=5; $i++) {writeUpcomingAssignment($i); } ?>
+				</div><!-- end upcomingAssignments -->
+				
+			</div><!-- end left hand side -->
 			
-			<div class="courseContentWrapper">
-				<?php writeDashWidgetTitle("Course Content"); ?>
+			<div id="rightHandSide" class="courseContent">		
+				<h2>Course Content </h2>
 				<div id="courseContent"></div>
 			</div>
-		
+
+			<div id="footer"></div>
 		</div>
-		
-		
-		
+
 		<!-- modal -->
-		<?php writeCalendarModal() ?>
+		<?php  writeCalendarModal(); ?>
+
 	</body>
 </html>
