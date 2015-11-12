ea56e32bee1a5e24d571f8485b7c0e8ae8e1ebb4
Jen Daniels <jdaniels@odu.edu>
2015-11-12 10:44:24 -0500

    added modal and assignments back

--- index.php
+++ index.php
@@ -5,17 +5,27 @@
 <!doctype html>
 <html>
 	<head>
-		<?php writeHead(""); ?>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="/mockups/css/reset.css">
+		<link rel="stylesheet" type="text/css" href="/mockups/fonts/font-awesome-4.4.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="/mockups/css/pleStyle.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link href='calendar/fullcalendar.css' rel='stylesheet' />
 		<link href='css/courseDashboard.css' rel='stylesheet'/>
+		<link href='css/modal.css' rel='stylesheet'/>
 		
+		
+		<script src="scripts/js/jquery-2.1.3.min.js"></script>
 		<script src='calendar/lib/moment.min.js'></script>
 		<script src='scripts/js/bootstrapmodal.min.js'></script>	
 		<script src='calendar/lib/jquery.min.js'></script>		
 		<script src="scripts/js/jquery-ui.min.js"></script>
 		<script src='calendar/fullcalendar.min.js'></script>
+		<script src="scripts/js/courseFunctions.js"></script>
 		
 		<script>
+
 		$(document).ready(function(){
 			makeCourseDashboard("cat101/json/kitten.json");
 			});
@@ -26,7 +36,6 @@
 		    }).done(function(obj) {
 		    	setTop(obj);
 		    	setCourseContent(obj);
-		    	
 				$('#eventContent').hide();
 				$('#calendar').fullCalendar({
 				    events: getEventsAtAGlance(obj),
@@ -37,8 +46,42 @@
 				        left: 'prev',
 				        center: 'title',
 				        right: 'next'
+				    },		    
+				    eventRender: function (event, element) {
+				        element.attr('href', 'javascript:void(0);');
+
+				        if(event.icon){
+				        	element.find(".fc-title").prepend("<i class='fa fa-" + event.icon + "'></a>");
+				        }
+
+				        
+				        element.click(function() {
+
+					        if (event.type == "assignment"){
+								$("#assignmentDeliverables").html(event.deliverable);
+						        $("#eventDateTime").html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
+								//these will not show if a module was clicked on first, so need to turn these back on
+								$("#eventIcon").show();
+								$("#assignmentDueHeader").show();
+								$("#assignmentDeliverableHeader").show();
+						        $("#assignmentDeliverables").show();
+						        $("#assignmentCompletionStatus").show();
+					        }
+					        else{
+								$("#eventIcon").hide();
+								$("#assignmentDueHeader").hide();
+						        $("#eventDateTime").html(moment(event.start).format('MMMM DD') + ' - ' + moment(event.end).format('MMMM DD, YYYY'));
+								$("#assignmentDeliverableHeader").hide();
+						        $("#assignmentDeliverables").hide();
+						        $("#assignmentCompletionStatus").hide();
+					        }
+					        $("#eventTitle").html(event.type + " - " + event.title);
+				            $("#eventDescription").html(event.description);
+				            $("#eventContent").dialog({ modal: true, width:500});
+
+				        });
 				    }
-			    });
+				});
 			});
 		};
 		</script>
