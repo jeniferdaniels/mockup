5a750c9f4ce5bb6d546e174f49c075a400cf4f69
Jen Daniels <jdaniels@odu.edu>
2015-12-04 19:31:28 -0500

    cleaned up schedule moved items from calendar directory to css or scripts directory added icons for chevron left and right, added themeButtonIcons cleaned up modal and renamed several items added scheduleListButton as a hack for the schedule listing. TODO: make button link to schedule list

--- schedule.php
+++ schedule.php
@@ -4,23 +4,51 @@
 <html>
 	<head>
 		<?php includeCsss() ?>
-		<link href='calendar/fullcalendar.css' rel='stylesheet' />
-		<link href='css/courseDashboard.css' rel='stylesheet'/>
+		<link href='css/fullcalendar.min.css' rel='stylesheet' />
+		
 
 		<?php includeScripts() ?>
-		<script src='calendar/lib/moment.min.js'></script>	
-		<script src='calendar/fullcalendar.min.js'></script>
+		<script src='scripts/js/moment.min.js'></script>	
+		<script src='scripts/js/fullcalendar.min.js'></script>
 		
 		<style>
 			.fc-center h2{
 				font-size: 1.4em;
-				margin-left: 100px;
+				margin-left: 100px !important;
+				font-family: "Open Sans", Georgia;
 			 }
+
+			 .fc-view-container{
+			 	border-bottom: 1px solid #AAAAAA;
+			 }
+			 
 			#eventContent{ 
 				z-index: 999; 
 			}
 			#wrapper{
 				margin: 150px auto 0;
+			}
+			
+			.ui-icon-odu_left-chevron{
+				background-image: url("images/leftChevron.svg") !important;
+				background-repeat: no-repeat;
+				background-size: 18px 18px;
+				height: 18px;
+				width: 18px;
+			}
+			
+			.ui-icon-odu_right-chevron{
+				background-image: url("images/rightChevron.svg") !important;
+				background-repeat: no-repeat;
+				background-size: 18px 18px;
+				height: 18px;
+				width: 18px;
+			}
+			.ui-state-default{
+				border: 1px solid #CCC;
+			}
+			.ui-state-active{
+				border: 1px solid black;
 			}
 		</style>
 		
@@ -47,38 +75,60 @@
 				        left: 'prev',
 				        center: 'title',
 				        right: 'month,basicWeek, next'
-				    },	    
+				    },
+				    themeButtonIcons: {
+						prev: 'odu_left-chevron',
+						next: 'odu_right-chevron',
+					},    
 				    eventRender: function (event, element) {
 				        element.attr('href', 'javascript:void(0);');
-				        element.click(function() {
 
+				      //event has properties: type, title, moduleNumber, start,	end, textColor,	description
+				      //if assignment also deliverable, submitVia, icon
+
+				      element.click(function() {
+					        $("#modalTitle").html(event.type);
+					        $('#modalTitle').css('textTransform', 'capitalize'); //upper case first letter of event type title
+							$('#description').html(event.description);
+					    	
 					        if (event.type == "assignment"){
-								$("#assignmentDeliverables").html(event.deliverable);
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
-								//these will not show if a module was clicked on first, so need to turn these back on
-								$("#eventIcon").show();
-								$("#assignmentDueHeader").show();
-								$("#assignmentDeliverableHeader").show();
-						        $("#assignmentDeliverables").show();
-						        $("#assignmentCompletionStatus").show();
+								$('#eventIcon').show();
+								$('#assignmentTitle').html(event.title);
+								$('#assignmentProperties').show();
+								$('#due').show();
+								$('#due').html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
+								$('#submitVia').show();
+								$('#submitVia').html(event.submitVia);
+								$('#deliverable').show();
+								$('#deliverable').html(event.deliverable);
+								$('#eventDate').hide(); //this is taken care of with the due date for an assignment
+								$('#checkmarkWrapper').show();
+								//TODO: cross check with list of completed items and mark accordingly	
 					        }
 					        else{
-								$("#eventIcon").hide();
-								$("#assignmentDueHeader").hide();
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD') + ' - ' + moment(event.end).format('MMMM DD, YYYY'));
-								$("#assignmentDeliverableHeader").hide();
-						        $("#assignmentDeliverables").hide();
-						        $("#assignmentCompletionStatus").hide();
-					        }
-					        $("#eventTitle").html(event.type + " - " + event.title);
-				            $("#eventDescription").html(event.description);
+					        	$("#pageTitle").append(event.moduleNumber);
+								$('#eventIcon').hide();	
+								$('#assignmentTitle').hide();	
+								$('#assignmentProperties').hide();
+								$('#eventDate').show();
+								$("#eventDateTime").html(moment(event.start).format('MMMM DD') + ' - ' + moment(event.end).format('MMMM DD, YYYY'));
+								$('#checkmarkWrapper').hide();
+						   }
+					       
 				            $("#eventContent").dialog({ modal: false, width:500});
 
 				        });
 				    }
 				});
 
+				$('.fc-toolbar .fc-right .fc-button-group').prepend(
+						$('<button id="scheduleListButton" class="fc-list-button ui-button ui-state-default ui-corner-left ui-state-default" type="button">list</button>'));
 
+				$('#scheduleListButton').hover(function(){
+					$('#scheduleListButton').toggleClass("ui-state-default");
+					$('#scheduleListButton').toggleClass("ui-state-hover");
+				});
+				
 				$("#editToolListItem").click(function(){				
 					$("body").toggleClass("editingMode");
 					$('#calendar').fullCalendar('editable', true);
@@ -105,8 +155,9 @@
 			<div class="wrapper">
 				<div id="courseMaterial">
 					<div class="paper">
-						<h2 id="pageTitle">Schedule</h2>
+						<h1 id="pageTitle">Schedule</h1>
 						<div id="calendar"></div>
+						<div id="jsonThingy"></div>
 					</div>					
 				</div>
 				
