db4332a1bbcd213fc6e2fb26e1142222f165428f
jdanielsODU <jdaniels@odu.edu>
2016-03-10 15:16:54 -0500

    moved calendar into lib/cal, updated references moved embedded css into function that writes the small calendar because they still need to be embedded in the page since they are overwriting fullcalendar styles added scheduleUrl as a variable which gets passed to the small calendar function to write a link under it. moved calendar script to lib/calendar/js removed upcoming assignments for now. removed title for calendar since it is obvious it is a calendar.

--- index.php
+++ index.php
@@ -6,25 +6,25 @@
 <html>
 	<head>
 		<?php includeCsss() ?>
-		<link href='calendar/fullcalendar.css' rel='stylesheet' />
-		<link href='css/courseDashboard.css' rel='stylesheet'/>
+		<link href="lib/calendar/css/fullcalendar.min.css" rel="stylesheet" />
+		<link href="lib/calendar/css/odu_calendar.css" rel="stylesheet" />
+		
+		<link href="css/courseDashboard.css" rel="stylesheet"/>
 
 		<?php includeScripts() ?>
-		<script src='calendar/lib/moment.min.js'></script>	
-		<script src='calendar/fullcalendar.min.js'></script>
-		<style>
-			.fc-time{
-				display : none;
-			}
-			.fc-day-number{
-				font-size: .7rem;
-			}
-		</style>
-
-		
+		<script src="lib/calendar/js/moment.min.js"></script>	
+		<script src="lib/calendar/js/fullcalendar.min.js"></script>
+		<script src="lib/calendar/js/odu_populateCalendar.js"></script>
+	
 		<script>
-
 		$(document).ready(function(){
+
+			var scheduleUrl = "schedule.php";
+			writeSmallCalendar("lib/calendar/sampleCalendarEvents.json", scheduleUrl);
+
+
+
+			
 			makeCourseDashboard("cat101/json/kitten.json");
 			displayChecksForCompletedAssignments("cat101/json/courseStatus.json");
 			});
@@ -65,57 +65,10 @@
 					$("#module_42_expanded").toggleClass("displayNone");	
 				});	
 
-
-		    	
+    	
 				$('#eventContent').hide();  //modal
 				
-				$('#calendar').fullCalendar({
-				    events: getEventsAtAGlance(obj),
-				    height: 420,
-				    fixedWeekCount: false,
-				    defaultDate: '2015-01-18',
-				    header: {
-				        left: 'prev',
-				        center: 'title',
-				        right: 'next'
-				    },		    
-				    eventRender: function (event, element) {
-				        element.attr('href', 'javascript:void(0);');
-
-				        if(event.icon){
-				        	//element.find(".fc-title").prepend("<i class='material-icons md-small'>" + event.icon + "</i></a>");
-				        	element.find(".fc-title").prepend("<i class='material-icons md-m'>" + event.icon + "</i></a>");
-
-				        }
-
-				        
-				        element.click(function() {
-
-					        if (event.type == "assignment"){
-								$("#assignmentDeliverables").html(event.deliverable);
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD, YYYY h:mm A'));
-								//these will not show if a module was clicked on first, so need to turn these back on
-								$("#eventIcon").show();
-								$("#assignmentDueHeader").show();
-								$("#assignmentDeliverableHeader").show();
-						        $("#assignmentDeliverables").show();
-						        $("#assignmentCompletionStatus").show();
-					        }
-					        else{
-								$("#eventIcon").hide();
-								$("#assignmentDueHeader").hide();
-						        $("#eventDateTime").html(moment(event.start).format('MMMM DD') + ' - ' + moment(event.end).format('MMMM DD, YYYY'));
-								$("#assignmentDeliverableHeader").hide();
-						        $("#assignmentDeliverables").hide();
-						        $("#assignmentCompletionStatus").hide();
-					        }
-					        $("#eventTitle").html(event.type + " - " + event.title);
-				            $("#eventDescription").html(event.description);
-				            $("#eventContent").dialog({ modal: false, width:500});
-
-				        });
-				    }
-				});
+				
 
 			});
 		};
@@ -128,21 +81,14 @@
 	
 		<div class="contentWrapper dash" id="dash">
 			<div id="leftHandSide">
-				<div class="scheduleAtAGlance">
-					<h2>Schedule At a Glance</h2>
-					<div id='calendar'></div>
-					<a href="schedule.php">Full Schedule</a>
+				<div id="odu_smallCalendarContainer">
+					<div id="odu_smallCalendar"></div>
 				</div>
 				
-				<div class="upcomingAssignments">
-					<h2>Upcoming Assignments</h2>
-						<?php for ($i=0; $i<=5; $i++) {writeUpcomingAssignment($i); } ?>
-				</div><!-- end upcomingAssignments -->
-				
+				<div class="odu_upcomingAssignments"></div>				
 			</div><!-- end left hand side -->
 			
 			<div id="rightHandSide" class="courseContent">		
-				<h2>Course Content </h2>
 				<div id="courseContent"></div>
 			</div>
 
@@ -151,11 +97,6 @@
 
 		<!-- modal -->
 		<?php  writeCalendarModal(); ?>
-
-
-
 		<div id="poop"></div>
-
-
 	</body>
 </html>
