6ed76981a893a38b81c0c99a84160b5ed640e618
Jen Daniels <jdaniels@odu.edu>
2016-04-06 16:36:27 -0400

    added messages and pnotify messages

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
 		<?php includeCsss() ?>
 		<link href="css/calendar/fullcalendar.min.css" rel="stylesheet" />
 		<link href="css/calendar/odu_calendar.css" rel="stylesheet" />
+		<link href="css/messages.css" rel="stylesheet"/>
 		<link href="css/courseDashboard.css" rel="stylesheet"/>
+		<link href="css/pnotify.custom.css" rel="stylesheet"/>
 
 		<?php includeScripts() ?>
 		<script src="scripts/js/utils.js"></script>
 		<script src="scripts/js/moment.min.js"></script>	
 		<script src="scripts/js/calendar/fullcalendar.min.js"></script>
 		<script src="scripts/js/calendar/odu_calendar.js"></script>
+		<script src="scripts/js/pnotify.custom.js"></script>
 	
 
 		<script>
@@ -22,8 +25,40 @@
 
 			var scheduleUrl = "schedule.php";
 			writeSmallCalendar("sampleJson/sampleCalendarEvents.json", scheduleUrl);
+
+
+
+			
 			makeCourseDashboard("cat101/json/kitten.json");
 			displayChecksForCompletedAssignments("cat101/json/courseStatus.json");
+			
+			new PNotify({
+				title: 'New Thing',
+				text: 'Just to let you know, something happened and it was a notice.',
+				hide: false
+			});
+			new PNotify({
+				title: 'New Thing',
+				text: 'Just to let you know, something happened and it was some info.',
+				type: 'info',
+				hide: false				
+			});
+			new PNotify({
+				title: 'New Thing',
+				text: 'Just to let you know, something happened and it was a success.',
+				type: 'success',
+				hide: false
+			});
+			new PNotify({
+				title: 'New Thing',
+				text: 'Just to let you know, something happened and it was an error.',
+				type: 'error',
+				hide: false
+			});
+			
+			
+			
+			
 			});
 		
 		function makeCourseDashboard(url) {
@@ -77,7 +112,12 @@
 		<div class="top"><?php writeTopHTML() ?></div>
 	
 		<div class="contentWrapper dash" id="dash">
-				<div id="leftHandSide">
+			<div id="announcement" class="odu_msgError"><div class="odu_msgClose" id="close_announcement">X</div>This is a sample error message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aajdshf dsajfkh askjh askjdfskadf kj kjasdhfkjashdfk;j a s a sjkfa asfjksfhak  jsahdf ka kjsfh;askdjh;aksjdh ksa  kjsdfhkasdh f;ksdafh ask haksfkjsdhfk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique.</div>
+			<div id="announcement1" class="odu_msgSuccess">This is a sample success message.</div>
+			<div id="announcement2" class="odu_msgWarning">This is a sample warning message.</div>
+			<div id="announcement3" class="odu_msgInfo">This is a sample infomational message.</div>			
+		
+			<div id="leftHandSide">
 				<div id="odu_smallCalendarContainer">
 					<div id="odu_smallCalendar"></div>
 				</div>
