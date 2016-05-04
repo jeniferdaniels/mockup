cbf94ba2be5387feb4b731179c19951034e5d6d8
Jen Daniels <jdaniels@odu.edu>
2016-05-04 19:48:28 -0400

    updated reference to css and js for upcoming events added code to get upcoming events from api updated container name for upcoming events

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<?php includeCsss() ?>
 		<link rel="stylesheet" href="css/calendar/fullcalendar.min.css"  />
 		<link rel="stylesheet" href="css/calendar/odu_calendar.css" />
-		<link rel="stylesheet" href="css/upcomingEvents/odu_upcomingEvents.css" />
+		<link rel="stylesheet" href="css/upEvents/odu_upEvents.css" />
 		<link rel="stylesheet" href="css/courseLandingStyle.css" />
 		<link rel="stylesheet" href="css/pnotify.custom.css" />
 
@@ -19,6 +19,7 @@
 		<script src="scripts/js/moment.min.js"></script>	
 		<script src="scripts/js/calendar/fullcalendar.min.js"></script>
 		<script src="scripts/js/calendar/odu_calendar.js"></script>
+		<script src="scripts/js/upEvents/odu_upEvents.js"></script>
 		<script src="scripts/js/pnotify.custom.js"></script>
 		<script src="scripts/js/messages/odu_messages.js"></script>
 
@@ -28,6 +29,16 @@
 			//calendar
 			writeSmallCalendar("sampleJson/sampleSmallCalendarEvents.json");
 			
+			//upcoming events
+			$.ajax({
+				url: 'http://ple1.odu.edu:4243/api/event/201530/comm270a',
+				type: 'GET',
+				dataType: 'json',
+				success: function(data) { writeUpEvents(data)},
+				error: function() { console.log("There was an error getting the upcomingEvents"); },
+				xhrFields: { withCredentials: true	},
+				crossDomain: true
+			});
 			
 			
 			
@@ -70,11 +81,9 @@
 						<div id="odu_smallCalendar"></div>
 					</div>
 					
-					<div id="odu_upEventsWrapper" class="wrapper">
+					<div id="odu_upEventsContainer" class="wrapper">
 						<h1 class="upEvents">Upcoming Events</h1>
-						<div style="padding: 10px">
-						
-					</div>
+						<div style="odu_upEventsWrapper" id="odu_upEventsWrapper"></div>
 					</div>
 				</div>
 				<!--end left hand side-->
