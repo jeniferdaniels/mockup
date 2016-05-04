919c1e94deff5bd653669a6f238759d3ce286dd2
Jen Daniels <jdaniels@odu.edu>
2016-05-04 15:11:23 -0400

    gutted. redid function to add small calendar since there is less json data coming and it is flat events not nested

--- scripts/js/calendar/odu_calendar.js
+++ scripts/js/calendar/odu_calendar.js
@@ -1,47 +1,3 @@
-//*****************************************************************************************
-//Function Name: populateCalendar()
-//Input: 		 url with loacation of json file 	 
-//Output:		 none, fills an html dom element
-//Purpose:		 calls the fullCalendar function and populates the #odu_calendar object
-//				 on the webpage
-//*****************************************************************************************
-function populateCalendar(url) {
-	
-	var noEventsMessage = "Because your instructor has not added any deadlines, or he/she is using another calendar, this calendar view has no events.";
-
-	$.ajax({ 
-		url: url
-	}).done(function(obj){
-		
-		//console.log("obj.events type is" + typeof obj.events);
-		
-		if (obj.events == "")
-			$('#calendarWrapper').before($("<div>").attr("id", "noEvents").html(noEventsMessage));
-		
-		$('#calendar').fullCalendar({
-			events: obj.events,
-			height: 770,
-			defaultDate: moment().format('YYYY-MM-DD'),
-			theme: true,
-			header: {
-				left: 'prev',
-				center: 'title',
-				right: 'month,basicWeek, next'
-			},
-			themeButtonIcons: {
-				prev: 'odu_left-chevron',
-				next: 'odu_right-chevron'
-			},
-			eventRender: function (event, element) {
-				element.attr('href', 'javascript:void(0);');
-				element.click(function() {
-					writeEventPopUp(event);
-					$("#odu_calendarPopUp").dialog({ modal: false, width:600, height: 500});
-				});				
-			}
-		});
-	});
-}
 
 //*****************************************************************************************
 //Function Name: writeSmallCalendar()
@@ -54,21 +10,21 @@ function writeSmallCalendar(url, scheduleUrl) {
 	$.ajax({ 
 		url: url
 	}).done(function(obj){
-		
+		console.log(dump(obj));
 		//override these calendar.op these styles specifically for the small calendar
 		$("<style type='text/css'> .fc-time{display: none} .fc-day-number{font-size: .7rem}</style>").appendTo("head");
 		
 		//add link to full schedule
-		$("#odu_smallCalendarContainer").after($("<a>").attr("href", scheduleUrl).html("Full Schedule"));
+		//$("#odu_smallCalendarContainer").after($("<a>").attr("href", scheduleUrl).html("Full Schedule"));
 		
 		//add divs for pop up
-		$("#odu_smallCalendarContainer").append($("<div>").attr("id", "odu_calendarPopUp").attr("class","displayNone").css("z-index", "30000").attr("title", "Assignment Details"));
-		$("#odu_calendarPopUp").append($("<div>").attr("id", "odu_popUpContentWrapper"));
-		$("#odu_popUpContentWrapper").append($("<div>").attr("id", "odu_popUpContent"));
+		//$("#odu_smallCalendarContainer").append($("<div>").attr("id", "odu_calendarPopUp").attr("class","displayNone").css("z-index", "30000").attr("title", "Assignment Details"));
+		//$("#odu_calendarPopUp").append($("<div>").attr("id", "odu_popUpContentWrapper"));
+		//$("#odu_popUpContentWrapper").append($("<div>").attr("id", "odu_popUpContent"));
 
 		//add small calendar
 		$("#odu_smallCalendar").fullCalendar({
-			events: setShortTitleEvents(obj), //set short title for small calendar
+			events: formatForSmallCal(obj), //set short title for small calendar
 			height: 430,
 			fixedWeekCount: true,
 			defaultDate: moment().format('2016-01-18'), //defaultDate: moment().format('YYYY-MM-DD'),
@@ -84,8 +40,6 @@ function writeSmallCalendar(url, scheduleUrl) {
 			},
 			eventRender: function (event, element) {
 				element.attr('href', 'javascript:void(0);');
-				if(event.icon)
-					element.find(".fc-title").prepend("<i class='smallAssignmentIcon'></i></a>");
 				element.click(function() {
 					writeEventPopUp(event);
 					$("#odu_calendarPopUp").dialog({ modal: false, width:600, height: 500});
@@ -97,143 +51,67 @@ function writeSmallCalendar(url, scheduleUrl) {
 	});
 }
 
-//*****************************************************************************************
-//Function Name: writeEventPopUp()
-//Input: 		 event object which has the following properties:
-//					type (example "assignment" or "module")	
-//					title (string)
-//					start (date, formatted YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS)
-//					end (date, same format as above, for assignments leave off end date)
-//					textColor (string, rgba, hex or htmt reserved word such as 'white'		
-//					description (string)
-//					if the type is "assignment" then it also has the following:
-//						submitVia (string)
-//						deliverable (string)
-//						icon (string)
-//						moreUrl (string) to the assignment's full page
-//Output:		 none, fills in a popup
-//Purpose:		 takes an event object and fills in objects on the dom with the event
-//				 object properties
-//*****************************************************************************************
-function writeEventPopUp(event){
-	//debug
-	showObjValuesInConsole(event);
-	
-	//it may contain residual items
-	$("#odu_popUpContent").empty();
+
+
+
+//gets object with keys:
+//type, title, longTitle, start, end, url  [module or other event]
+//type, title, longTitle, due, url [assignment]
+//and adds keys color, text color (different ones depending on module or assignment)
+//changes name of dueDate property on assignments to start so calendar can consume it 
+//correctly
+
+function formatForSmallCal(eventList){
+	//TODO: fix this later
+	var assTextColor = "white";
+	var assColor = "rgba(68,136,246,1)";
+	var modTextColor = "black";
+	var modColor = "rgba(240,240,240,1)";
 
 	
-	if (event.type == "assignment"){
-		$("#odu_popUpContent").append($("<div>").attr("id", "eventIcon").addClass("assignmentIcon"));
-		$("#odu_popUpContent").append($("<h1>").attr("id", "modalTitle").html("Assignment"));
-		$("#odu_popUpContent").append($("<h3>").attr("id", "assignmentTitle").html(event.longTitle));
-		$("#odu_popUpContent").append($("<ul>").attr("id", "assignmentProperties").addClass("assignmentProperties"));
-		
-		if (!(typeof event.start === 'undefined')){
-			if (event.start != ""){
-			$("#assignmentProperties").append($("<li>").attr("id", "dueItem").append($("<div>").attr("id", "dueHeader").html("Due-")));
-			$("#dueItem").append($("<div>").attr("id", "due").html((moment(event.start).format('MMMM DD, YYYY h:mm A'))));
-			}
-		}
-		
-		if (!(typeof event.submitVia === 'undefined')){
-			if (event.submitVia != ""){ //wont work any other way
-				$("#assignmentProperties").append($("<li>").attr("id", "submitViaItem").append($("<div>").attr("id", "submitViaHeader").html("Submit-")));
-				$("#submitViaItem").append($("<div>").attr("id", "submitVia").html(event.submitVia));
-			}
-		}
-		
-		if (!(typeof event.deliverable === 'undefined')){
-			if (event.deliverable != "") {
-				$("#assignmentProperties").append($("<li>").attr("id", "deliverableItem").append($("<div>").attr("id", "deliverableHeader").html("Deliverable-")));
-				$("#deliverableItem").append($("<div>").attr("id", "deliverable").html(event.deliverable));				
-			}
-		}		
-		
-		if (!(typeof event.description === 'undefined')){
-			if (event.description != "") {
-				$("#assignmentProperties").append($("<p>").attr("id", "description").html(event.description));				
-			}
-		}
-		
-		$("#odu_popUpContent").append($("<div>").attr("id", "clear").attr("style", "clear:both"));
-		
-		if (!(typeof event.moreUrl === 'undefined')){
-			if (event.moreUrl != "") {
-				$("#odu_popUpContent").append($("<div>").append($("<a>").attr("id", "moreLink").attr("href", event.moreUrl).html("View more on module assignment page...")));
+	if (typeof eventList != "undefined"){
+		for (var i=0; i< eventList.length; i++){
+			var obj = {}; //clear out any residule
+			obj = eventList[i];
+			
+			if (typeof obj != "undefined"){
+				obj = changeKeyName(obj, "due", "start");
+			
+				//TODO: fix this so its not so stupid 
+				if (obj.type == "module"){
+					obj = addKeyValuePair(obj, "color", modColor);
+					obj = addKeyValuePair(obj, "textColor", modTextColor);
+				}
+				else{
+					obj = addKeyValuePair(obj, "color", assColor);
+					obj = addKeyValuePair(obj, "textColor", assTextColor);
+				}
+			console.log(dump(obj));
 			}
 		}
 	}
-	//write code here for Module Info popup (need to grab objectives, description etc which are not avaiable in the JSON file yet)
+
+	console.log(dump(eventList));
+	return eventList;
 }
 
-
-
-//*****************************************************************************************
-//Function Name: setShortTitleEvents()
-//Input: 		 event object which has the following properties:
-//					type (example "assignment" or "module")	
-//					title (string)
-//					start (date, formatted YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS)
-//					end (date, same format as above, for assignments leave off end date)
-//					textColor (string, rgba, hex or htmt reserved word such as 'white'		
-//					description (string)
-//					if the type is "assignment" then it also has the following:
-//						submitVia (string)
-//						deliverable (string)
-//						icon (string)
-//						moreUrl (string) to the assignment's full page	 
-//Output:		 array of objects with shortTitle, longTitle and regularTitle
-//Purpose:		 Takes conglomerate of info and returns only title items, if there is a short
-//				 title sent, call that the title.  Make a property called long title and set
-//				 the title to to.  This is a work around for the small calendar because it 
-//				 still calls the obj.title and we need to make a short title without changing
-//				 the code for the fullcalendar.io.
-//**********************************************************************************************
-
-
-function setShortTitleEvents(obj)
-{
-	var shortEvents = [];
-	var shortTitleEvent = {};
-	
-	
-	for (var i=0; i<obj.events.length; i++){
-		var orgEvent = obj.events[i];
-		
-		shortTitleEvent = {}; //clear it out
-		shortTitleEvent = orgEvent;
-		
-		if (orgEvent.shortTitle != ""){
-			shortTitleEvent.longTitle = orgEvent.title;
-			shortTitleEvent.title = orgEvent.shortTitle;
-			}
-		else{
-			shortTitleEvent.longTitle = orgEvent.title;
-			shortTitleEvent.title = "";
+//changes the key name
+//if the key is not there, it returns the object unaltered
+function changeKeyName(obj, currentName, newName){
+	if (typeof obj != "undefined"){
+		if (obj.hasOwnProperty(currentName)){
+			var newValue = obj[currentName];
+			obj[newName] = newValue;
+			delete obj[currentName];
 		}
-		shortEvents.push(shortTitleEvent);
 	}
-
-	return shortEvents;
-
+	return obj;
 }
 
 
-//*****************************************************************************************
-//Function Name: writeUpcomingEvents()
-//Input: 		 event object which has the following properties:
-//					type (example "assignment" or "module")	
-//					title (string)
-//					start (date, formatted YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS)
-//					end (date, same format as above, for assignments leave off end date)
-//					textColor (string, rgba, hex or htmt reserved word such as 'white'		
-//					description (string)
-//					if the type is "assignment" then it also has the following:
-//						submitVia (string)
-//						deliverable (string)
-//						icon (string)
-//						moreUrl (string) to the assignment's full page	 
-//Output:		 array of objects with longTitle, date and description
-//Purpose:		 
-//*******************************************************************************************
+function addKeyValuePair(obj, key, value){
+	if (typeof obj != "undefined"){
+		obj[key] = value;
+	}
+	return obj;
+}
