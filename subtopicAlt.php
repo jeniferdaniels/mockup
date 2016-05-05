09bdfe843ef0cdc2384999dabebfc773f5498470
Jen Daniels <jdaniels@odu.edu>
2016-05-04 22:47:40 -0400

    fixes json, so removed kludge added breadcrumbs removed progressBarWrapper

--- subtopicAlt.php
+++ subtopicAlt.php
@@ -19,8 +19,6 @@
 					//***************************************
 					//set page content
 					//***************************************
-					setTop(obj);
-					document.title = getCourseTitle(obj);
 					flatCourse = flattenCourse(obj);
 
 					var subtopicId = (typeof $.QueryString["id"] !== undefined) ? $.QueryString["id"] : "3"; //default it to something for now 
@@ -30,13 +28,7 @@
 					grandParent = getObjectFromArray(flatCourse, "id", parent.parent);
 					previous = flatCourse[flatCourse.indexOf(subtopic)-1];
 					next = flatCourse[flatCourse.indexOf(subtopic)+1];
-					//TODO: kludge so I dont have to open the json file to either change the url or add
-					//an alt url or take the querystring part off the url
-					//useArrowsForNavigation(previous.url, next.url);
-					prevUrl = "subtopicAlt.php?id=" + previous.id;
-					nextUrl = "subtopicAlt.php?id=" + next.id;
-					console.log("nav urls Prev:" + prevUrl + " Next: " + nextUrl);
-					useArrowsForNavigation(prevUrl, nextUrl);
+
 										
 					
 					
@@ -46,8 +38,10 @@
 					$("#moduleCrumb").html("<a href='index.php'>" + grandParent.displayNumber + ". " + grandParent.title + "</a>");
 					$("#topicCrumb").html("<a href='" + parent.url + "'>" + parent.displayNumber + " " + parent.title + "</a>");							
 					$("#thisCrumb").html(subtopic.displayNumber + " " + subtopic.title); 
-					$("#prev").prop("href", prevUrl);
-					$("#next").prop("href", nextUrl);
+					$("#prev").prop("href", previous.url);
+					$("#next").prop("href", next.url);
+					console.log("nav urls Prev:" + previous.url + " Next: " + next.url);
+					useArrowsForNavigation(previous.url, next.url);
 					
 					$("#dropDownMenu").hide();
 					//toolBoxFunctionality();																				
@@ -56,7 +50,7 @@
 		</script>
 	</head>
 	
-	<body class="bodyAlt">
+	<body>
 		<div id="popWindow" class="displayNone"><?php writeDummyGlossaryTerms(10)?></div>
 	
 		<div class="navWrapper">
@@ -68,16 +62,25 @@
 		<div class="top" id="top"><?php writeTopHtml(); ?></div>
 		
 
-		<div class="odu_wrapper odu_wrapperAlt" id="odu_wrapper">
+		<div class="odu_wrapper" id="odu_wrapper">
 			<div class="odu_titleAlt"><h1 id="pageTitle"></h1></div>
+			<div class="odu_breadCrumbWrapperAlt">
+				<nav>
+					<ul class="odu_breadCrumbs" id="odu_breadCrumbs">
+						<li id="moduleCrumb"></li>
+						<li id="topicCrumb"></li>
+					</ul>
+				</nav>
+			</div>
 			
 			<div class="odu_toolbox odu_toolboxAlt" id="odu_toolbox">
 				<?PHP writeToolsAlt(1) ?>
 			</div>
 			<div class="odu_content odu_contentAlt" id="odu_content">
-				<div class="odu_progressBarWrapper" id="odu_progressBarWrapper"></div>
+
 				<div class="odu_paper odu_paperAlt" id="odu_paper">
 					<div class="fakeEditor" id="fakeEditor"><img src="<?PHP echo WEB_ROOT ?>/images/CKEditorSample.png"></div>
+
 					<?php writeProgressNavBar() ?>
 						
 						
