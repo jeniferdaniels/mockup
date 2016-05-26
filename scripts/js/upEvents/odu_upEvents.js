9e125ae8c8065b0dae6d3246e77c1273def0930e
Jen Daniels <jdaniels@odu.edu>
2016-05-25 20:15:59 -0400

    removed debug added jquery list

--- scripts/js/upEvents/odu_upEvents.js
+++ scripts/js/upEvents/odu_upEvents.js
@@ -1,52 +1,53 @@
 //*****************************************************************************************
 //Function Name: writeUpEvents()
-//Input: 		 url with location of json file 		 
+//Input: 		 eventList - json file 
+//				 eventListContainerId - id for where to put this list		 
 //Output:		 none, fills an html dom element
 //Purpose:		 writes events in nice format for landing page, assumes landing page div is 
 //Dependencies:	 needs utils.js function hasEmptyKeys()
 //*****************************************************************************************
-function writeUpEvents(eventList) {
+function writeUpEvents(eventList, eventListContainerId) {
+	eventListUlId = "odu_upEvents";
+	
 
+	console.log("here");
 	if (typeof eventList != "undefined"){
 		var expectedKeys = ["type", "title", "longTitle", "id", "theDate", "url", "description"];
 		
-		for (var i=0; i< eventList.length; i++){
-			var theEvent = []; //clear out any residule
-			theEvent = eventList[i];
-			theEvent = changeKeyName(theEvent, "dueDate", "theDate");	//to make js not barf
-			theEvent = changeKeyName(theEvent, "date", "theDate");	//to make js not barf
-			
-			if (!hasEmptyKeys(theEvent, expectedKeys)){
-				eventDivId = "event_" + i;
+		if (eventList.length > 0){
+
+			$("#" + eventListContainerId).append($("<ul>").attr("id", eventListUlId));
+		
+			for (var i=0; i< eventList.length; i++){
+				var theEvent = []; //clear out any residule
+				theEvent = eventList[i];
+				theEvent = changeKeyName(theEvent, "dueDate", "theDate");	//to make js not barf
+				theEvent = changeKeyName(theEvent, "date", "theDate");	//to make js not barf
 				
-				$("#odu_upEventsWrapper").append(
-					$("<div>").addClass("odu_upEvent").attr("id", eventDivId).append(
-						$("<div>").addClass("dateTitleWrapper").append(
-							$("<div>").addClass("date").html(moment(theEvent.theDate).format("MMM")).append(
-								$("<h2>").html(moment(theEvent.theDate).format("DD"))
+				if (!hasEmptyKeys(theEvent, expectedKeys)){
+					eventId = "event_" + i;
+					
+					$("#" + eventListUlId).append(
+						$("<li>").attr("id", eventId).addClass("odu_upEvent").append(
+							$("<div>").addClass("dateBox").append(
+								$("<div>").addClass("dateBoxMonth").html(moment(theEvent.theDate).format("MMM"))
 							).append(
-								$("<time>").html(moment(theEvent.theDate).format("h:MM A"))
+								$("<div>").addClass("dateBoxDay").html(moment(theEvent.theDate).format("DD"))
+							).append(
+								$("<div>").addClass("dateBoxTime").html(moment(theEvent.theDate).format("h:MM A"))
 							)
 						).append(
-							$("<h3>").append(
-								$("<a>").attr("href", theEvent.url).html(theEvent.title + " " + theEvent.longTitle)
-							)
-						).append(
-							$("<div>").addClass("clearFix")
-						).append(
-							$("<p>").html(theEvent.description).append(
-								$("<a>").attr("href", theEvent.url).html("More >")
+							$("<div>").append(
+								$("<h1>").html(theEvent.title + " " + theEvent.longTitle)
+							).append(
+								$("<p>").html(theEvent.description).append(
+									$("<a>").html("More...").attr("href", theEvent.url)
+								)
 							)
 						)
-					)
-				);
-
-				
-				//string = '<div class="odu_upEvent" id="event_5"><div class="dateTitleWrapper"><div class="date">Jan<h2>18</h2><time>11:59 pm</time></div><h3><a href="#">Assignment: A.1 itle Here dfjk  sdkjf sakdjfl ksjdflk dasfj sdljflska jsdkjdlsj</a></h3></div><div class="clearFix"></div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula</p><a href="#">More &gt;</a></div>';
-				//$("#odu_upEventsWrapper").html(string);
-				
+					);
+				}
 			}
-
 		}
 	}
 }
