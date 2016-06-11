5dcc261604757370f13bb3ac4755c3b1e7127dee
Jen Daniels <jdaniels@odu.edu>
2016-06-10 20:49:36 -0400

    reformatted code display fixed ul and li nesting

--- scripts/js/upEvents/odu_upEvents.js
+++ scripts/js/upEvents/odu_upEvents.js
@@ -7,18 +7,15 @@
 //Dependencies:	 needs utils.js function hasEmptyKeys()
 //*****************************************************************************************
 function writeUpEvents(eventList, eventListContainerId) {
-	eventListUlId = "odu_upEvents";
-	
 
-	console.log("here");
 	if (typeof eventList != "undefined"){
 		var expectedKeys = ["type", "title", "longTitle", "id", "theDate", "url", "description"];
 		
 		if (eventList.length > 0){
-
-			$("#" + eventListContainerId).append($("<ul>").attr("id", eventListUlId));
+			$(eventListContainerId).addClass("wrapper").append($("<ul>").attr("id","odu_upEvents"));
 		
 			for (var i=0; i< eventList.length; i++){
+				//$("#odu_upEventsLists").append("<li>");
 				var theEvent = []; //clear out any residule
 				theEvent = eventList[i];
 				theEvent = changeKeyName(theEvent, "dueDate", "theDate");	//to make js not barf
@@ -27,27 +24,19 @@ function writeUpEvents(eventList, eventListContainerId) {
 				if (!hasEmptyKeys(theEvent, expectedKeys)){
 					eventId = "event_" + i;
 					
-					$("#" + eventListUlId).append(
-						$("<li>").attr("id", eventId).addClass("odu_upEvent").append(
-							$("<div>").addClass("dateBox").append(
-								$("<div>").addClass("dateBoxMonth").html(moment(theEvent.theDate).format("MMM"))
-							).append(
-								$("<div>").addClass("dateBoxDay").html(moment(theEvent.theDate).format("DD"))
-							).append(
-								$("<div>").addClass("dateBoxTime").html(moment(theEvent.theDate).format("h:MM A"))
-							)
-						).append(
-							$("<div>").append(
-								$("<h1>").html(theEvent.title + " " + theEvent.longTitle)
-							).append(
-								$("<p>").html(theEvent.description).append(
-									$("<a>").html("More...").attr("href", theEvent.url)
-								)
-							)
-						)
-					);
+					$("#odu_upEvents").append(
+						$("<li>").attr("id", eventId).addClass("odu_upEvent")
+						.append($("<div>").addClass("dateBox")
+							.append($("<div>").addClass("dateBoxMonth").html(moment(theEvent.theDate).format("MMM")))
+							.append($("<div>").addClass("dateBoxDay").html(moment(theEvent.theDate).format("DD")))
+							.append($("<div>").addClass("dateBoxTime").html(moment(theEvent.theDate).format("h:MM A"))))
+							.append($("<div>")
+						.append($("<h1>").html(theEvent.title + " " + theEvent.longTitle))
+							.append($("<p>").html(theEvent.description)
+								.append($("<a>").html("More...").attr("href", theEvent.url)))));
 				}
 			}
+			
 		}
 	}
 }
