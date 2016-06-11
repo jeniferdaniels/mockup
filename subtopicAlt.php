143c09cac7d615126839db80d3a6c9a73f5edbc3
Jen Daniels <jdaniels@odu.edu>
2016-06-10 21:10:32 -0400

    removed all php, converted to hardcoded html removed arrow nav script removed breadcrumb generator

--- subtopicAlt.php
+++ subtopicAlt.php
@@ -1,91 +1,103 @@
-<?php include_once 'scripts/mockupFunctions.php' ?>
-<?php $subtopicId = (isset($_REQUEST['id'])? $_GET["id"] : "3");?>
+
 
 <!doctype html>
 <html>
 	<head>
-		<?php  includeCsss (); ?>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/pleStyle.css">	
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" >
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	
+	<link rel="stylesheet" type="text/css" href="css/basicContent.css">
+	<link rel="stylesheet" type="text/css" href="css/navArrowStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/modal.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui-1.10.4.custom.css">
+	<link rel="stylesheet" type="text/css" href="css/calendar/fullcalendar.min.css"  />
+	<link rel="stylesheet" type="text/css" href="css/courseHome.css" />
+	<link rel="stylesheet" type="text/css" href="css/courseHome.css" />
+	<link rel="stylesheet" type="text/css" href="css/calendar/odu_calendar.css" />
+	<link rel="stylesheet" type="text/css" href="css/upEvents/odu_upEvents.css" />
+	<link rel="stylesheet" type="text/css" href="css/moduleList/moduleListStyle.css" />
+	<link rel="stylesheet" type="text/css" href="css/pnotify.custom.css" />
+	
+	<script type="text/javascript" src="scripts/js/jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="scripts/js/jquery-ui.min.js"></script>
+	<!--<script type="text/javascript" src="<!-- string::webdir_assets --><!--js/_odu/courseFunctions.js"></script>	-->	
+	<script type="text/javascript" src="scripts/js/utils.js"></script>
+	<script type="text/javascript" src="scripts/js/moment.min.js"></script>	
+	<script type="text/javascript" src="scripts/js/calendar/fullcalendar.min.js"></script>
+	<script type="text/javascript" src="scripts/js/calendar/odu_calendar.js"></script>
+	<script type="text/javascript" src="scripts/js/upEvents/odu_upEvents.js"></script>
+	<script type="text/javascript" src="scripts/js/pnotify.custom.js"></script>
+	<script type="text/javascript" src="scripts/js/messages/odu_messages.js"></script>
+	<script type="text/javascript" src="scripts/js/personalization/odu_preferences.js"></script>
+	<script type="text/javascript" src="scripts/js/moduleList/odu_moduleList.js"></script>
+	<script type="text/javascript" src="scripts/js/odu_basicContent.js"></script>
 		
-		
-		<?php  includeScripts (); ?>
-		
-		
-		<script>
-			$(document).ready(function(){
-				$.ajax({
-					url: "cat101/json/kitten.json"
-				}).done(function(obj) {
-
-					//***************************************
-					//set page content
-					//***************************************
-					flatCourse = flattenCourse(obj);
-
-					var subtopicId = (typeof $.QueryString["id"] !== undefined) ? $.QueryString["id"] : "3"; //default it to something for now 
-					//TODO: make a default id and handle that
-					subtopic = getObjectFromArray(flatCourse, "id", subtopicId);					
-					parent = getObjectFromArray(flatCourse, "id", subtopic.parent);
-					grandParent = getObjectFromArray(flatCourse, "id", parent.parent);
-					previous = flatCourse[flatCourse.indexOf(subtopic)-1];
-					next = flatCourse[flatCourse.indexOf(subtopic)+1];
-
-										
-					
-					
-					
-					$("#pageTitle").html(subtopic.displayNumber + " " + subtopic.title);
-					$("#content").load("cat101/" + subtopic.content);
-					$("#moduleCrumb").html("<a href='index.php'>" + grandParent.displayNumber + ". " + grandParent.title + "</a>");
-					$("#topicCrumb").html("<a href='" + parent.url + "'>" + parent.displayNumber + " " + parent.title + "</a>");							
-					$("#thisCrumb").html(subtopic.displayNumber + " " + subtopic.title); 
-					$("#prev").prop("href", previous.url);
-					$("#next").prop("href", next.url);
-					console.log("nav urls Prev:" + previous.url + " Next: " + next.url);
-					useArrowsForNavigation(previous.url, next.url);
-					
-					$("#dropDownMenu").hide();
-					//toolBoxFunctionality();																				
-					});// done 
-				});//ready
-		</script>
 	</head>
 	
 	<body>
-		<div id="popWindow" class="displayNone"><?php writeDummyGlossaryTerms(10)?></div>
+		<div id="popWindow" class="displayNone">Glossary</div>
 	
 		<div class="navWrapper">
-			<div id="navPrevious" class="navPrevious"><a id="prev" href=""><?php echo(icon("fa", "previous", "e", "")); ?></a></div>
-			<div id="navNext" class="navNext"><a id="next" href=""><?php echo(icon("fa", "next", "e", "")); ?></a></div>
+			<div id="navPrevious" class="navPrevious"><a id="prev" href="#"><i class="fa fa-fw fa-angle-left fa-5x "></i></a></div>
+			<div id="navNext" class="navNext"><a id="next" href="#"><i class="fa fa-fw fa-angle-right fa-5x "></i>></a></div>
 		</div>
 	
 	
-		<div class="top" id="top"><?php writeTopHtml(); ?></div>
+		<div class="top" id="top">header</div>
 		
 
 		<div class="odu_wrapper" id="odu_wrapper">
-			<div class="odu_titleAlt"><h1 id="pageTitle"></h1></div>
+			<div class="odu_titleAlt"><h1 id="pageTitle">Page Title</h1></div>
 			<div class="odu_breadCrumbWrapperAlt">
 				<nav>
 					<ul class="odu_breadCrumbs" id="odu_breadCrumbs">
-						<li id="moduleCrumb"></li>
-						<li id="topicCrumb"></li>
+						<li id="moduleCrumb">Module Crumb</li>
+						<li id="topicCrumb">Topic Crumb</li>
+						<li id="subTopicCrumb">Subtopic Crumb</li>
 					</ul>
 				</nav>
 			</div>
 			
 			<div class="odu_toolbox odu_toolboxAlt" id="odu_toolbox">
-				<?PHP writeToolsAlt(1) ?>
-			</div>
+				<div id="odu_toolboxAlt" class="odu_toolboxAlt">
+					<ul id="odu_toolsAlt" class="odu_toolsAlt">
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Home</span></a></li>
+						<li><a href=="#"><i class="fa fa-2x fa-ban"></i><span>Syllabus</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Modules</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Calendar</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Ask A Question</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Notes</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Print</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Edit</span></a></li>
+						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Admin Tools</span></a></li>
+						</ul>
+					</div>
+				</div>
 			<div class="odu_content odu_contentAlt" id="odu_content">
-
-				<div class="odu_paper odu_paperAlt" id="odu_paper">
-					<div class="fakeEditor" id="fakeEditor"><img src="<?PHP echo WEB_ROOT ?>/images/CKEditorSample.png"></div>
-
-					<?php writeProgressNavBar() ?>
-						
-						
+				<div class="odu_paper" id="odu_paper">
+					<div class='odu_progressNavBar'>
+						<a href='#'></a>
+						<a href='#'></a>
+						<a href='#'></a>
+						<a href='#'></a>
+						<a href='#'></a>
+						<a href='#'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+						<a href='#' class='new'></a>
+					</div>
 					
-					<div class="odu_stuff" id="content"></div>
+					<div class="odu_stuff" id="content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique. 
+					</div>
 				</div>
 			</div>
 			
