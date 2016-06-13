b65714cf41a4caed4aa1790c5aee0ba74230d7e6
Jen Daniels <jdaniels@odu.edu>
2016-06-13 17:49:14 -0400

    cleaned up. decided to move breadcrumbs to top.   //TODO: put in header?

--- subtopic.php
+++ subtopic.php
@@ -63,77 +63,50 @@
 	</head>
 	
 	<body>
-		<div id="errorMsg" class="errorMsg"></div>
-		<!--add later <div id="popWindow" class="displayNone">Glossary</div>-->
-	
+
+	<div class="higherBreadCrumbWrapper">
+		<ul class="odu_breadCrumbs higherBreadcrumbs" id="odu_breadCrumbs">
+			<li>Module Crumb!!!!!!!!!!!!!!</li>
+			<li>Topic Crumb</li>
+			<li>Subtopic Crumb</li>
+		</ul>
+	</div>
+		
+	<!--look into moving this-->
 		<div class="navWrapper">
 			<div id="navPrevious" class="navPrevious"><a id="prev" href="#"><i class="fa fa-fw fa-angle-left fa-5x "></i></a></div>
 			<div id="navNext" class="navNext"><a id="next" href="#"><i class="fa fa-fw fa-angle-right fa-5x "></i>></a></div>
-		</div>
-	
-	
-		<div class="odu_top" id="odu_top"></div>
-		
+		</div>	
+	<!--add later <div id="popWindow" class="displayNone">Glossary</div>-->	
+		<div id="errorMsg" class="errorMsg"></div>
+		<div id="odu_top" class="odu_top"></div>
+		<div id="odu_wrapper" class="odu_wrapper">
+			<h1 id="odu_pageTitle" class="odu_pageTitle">Page Title</h1>
+						
 
-		<div class="odu_wrapper" id="odu_wrapper">
-			<h1 id="pageTitle">Page Title</h1>
 			
 			
-			
-			
-			
-			
-			<div class="odu_breadCrumbWrapperAlt">
-				<nav>
-					<ul class="odu_breadCrumbs" id="odu_breadCrumbs">
-						<li id="moduleCrumb">Module Crumb</li>
-						<li id="topicCrumb">Topic Crumb</li>
-						<li id="subTopicCrumb">Subtopic Crumb</li>
-					</ul>
-				</nav>
-			</div>
-			
-			<div class="odu_toolbox odu_toolboxAlt" id="odu_toolbox">
-				<div id="odu_toolboxAlt" class="odu_toolboxAlt">
-					<ul id="odu_toolsAlt" class="odu_toolsAlt">
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Home</span></a></li>
-						<li><a href=="#"><i class="fa fa-2x fa-ban"></i><span>Syllabus</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Modules</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Calendar</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Ask A Question</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Notes</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Print</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Edit</span></a></li>
-						<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Admin Tools</span></a></li>
-						</ul>
-					</div>
+			<nav class="odu_toolbox" id="odu_toolbox">
+				<ul id="odu_toolboxList" class="odu_toolboxList">
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Home</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Syllabus</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Modules</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Calendar</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Ask A Question</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Notes</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Print</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Edit</span></a></li>
+					<li><a href="#"><i class="fa fa-2x fa-ban"></i><span>Admin Tools</span></a></li>
+				</ul>
+			</nav>
+
+			<section id="odu_contentSection" class="odu_contentSection">
+				<div id="odu_contentWrapper" class="odu_contentWrapper">
+					<div id="odu_content" class="odu_content"></div>
 				</div>
-			<div class="odu_content odu_contentAlt" id="odu_content">
-				<div class="odu_paper" id="odu_paper">
-					<div class='odu_progressNavBar'>
-						<a href='#'></a>
-						<a href='#'></a>
-						<a href='#'></a>
-						<a href='#'></a>
-						<a href='#'></a>
-						<a href='#'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-						<a href='#' class='new'></a>
-					</div>
-					
-					<div class="odu_stuff" id="content">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum sem id mauris vehicula lobortis. Aliquam ut tortor odio. Curabitur cursus leo eu pellentesque consequat. Curabitur sapien nibh, vestibulum sed tortor eget, posuere rhoncus nibh. Curabitur efficitur tellus risus. Nullam sit amet massa ultrices lacus facilisis maximus cursus ac arcu. Maecenas eu nulla in orci porta pretium. Fusce placerat luctus posuere. Donec blandit ligula non malesuada tristique. 
-					</div>
-				</div>
-			</div>
+			</section>
 			
-			
+			<div class="clearFix"></div>
 			
 		</div>
 
