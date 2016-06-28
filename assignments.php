4e21fcd0bbd5394b7c64572737b30406f4c2b87c
Jen Daniels <jdaniels@odu.edu>
2016-06-28 18:36:24 -0400

    cleaned out php, added css and js includes created skeleton

--- assignments.php
+++ assignments.php
@@ -1,50 +1,75 @@
-<?php include_once 'scripts/mockupFunctions.php' ?>
-<?php //TODO: redo this page to use js ?>
-<?php 
-		
-	$showModuleProgress = 0;
-	$pageTitle = "Assignments";
-
-	$breadCrumbs = array(
-		array("url"=>"index.php", "displayTitle"=>"Home"),
-		array("url"=>"", "displayTitle"=>$pageTitle));
-	
-?>
 
 <!doctype html>
 <html>
-<head>
-		<?php  includeCsss (); ?>
-		
-		<?php  includeScripts (); ?>
-		
-		
-		<script>
-			$(document).ready(function(){
-				$.ajax({
-					url: "cat101/json/kitten.json"
-				}).done(function(obj) {
-
-					//***************************************
-					//set page content
-					//***************************************
-					setTop(obj);
-					document.title = getCourseTitle(obj);
-
-					
-					toolBoxFunctionality();																				
-					});// done 
-				});//ready
-		</script>
+	<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/pleStyle.css">	
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/ple-awesome/styles.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" />
+	
+	<link rel="stylesheet" type="text/css" href="css/navArrowStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/modal.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui-1.10.4.custom.css">
+	<link rel="stylesheet" type="text/css" href="css/calendar/fullcalendar.min.css"  />
+	<link rel="stylesheet" type="text/css" href="css/courseHome.css" />
+	<link rel="stylesheet" type="text/css" href="css/calendar/odu_calendar.css" />
+	<link rel="stylesheet" type="text/css" href="css/upEvents/odu_upEvents.css" />
+	<link rel="stylesheet" type="text/css" href="css/moduleList/moduleListStyle.css" />
+	<link rel="stylesheet" type="text/css" href="css/assignments/odu_assignments.css" />
+	<link rel="stylesheet" type="text/css" href="css/pnotify.custom.css" />
+	
+	<script>DEBUG = true;</script>
+	<script type="text/javascript" src="scripts/js/jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="scripts/js/jquery-ui.min.js"></script>
+	<!--<script type="text/javascript" src="<!-- string::webdir_assets --><!--js/_odu/courseFunctions.js"></script>	-->	
+	<script type="text/javascript" src="scripts/js/utils.js"></script>
+	<script type="text/javascript" src="scripts/js/moment.min.js"></script>	
+	<script type="text/javascript" src="scripts/js/calendar/fullcalendar.min.js"></script>
+	<script type="text/javascript" src="scripts/js/calendar/odu_calendar.js"></script>
+	<script type="text/javascript" src="scripts/js/upEvents/odu_upEvents.js"></script>
+	<script type="text/javascript" src="scripts/js/pnotify.custom.js"></script>
+	<script type="text/javascript" src="scripts/js/messages/odu_messages.js"></script>
+	<script type="text/javascript" src="scripts/js/personalization/odu_preferences.js"></script>
+	<script type="text/javascript" src="scripts/js/moduleList/odu_moduleList.js"></script>
+	
 	</head>
 	<body>
-		<div class="top" id="top"><?php writeTopHtml(); ?></div>
-	
-		<div class="contentWrapper" id="contentWrapper">
+
 			
-			<h2><?php echo $pageTitle ?></h2>
-			
-			assignments go here
-		</div>
+			<section id="odu_assignmentsSection">
+				<h1 class="odu_sectionH1 assignments">Assignments</h1>
+				<div class="odu_assignmentsWrapper" class="wrapper">
+					<ul id="odu_assignments">
+						<li id="odu_assignment_1">
+							<ul class="odu_assignmentDetails">
+								<li><h2>1.A.1 Review Required Materials</h2></li>
+								<li><h3>Due</h3><span>May 31, 2001 at 11:59 PM</span></li>
+								<li><h3>Submit Via</h3><span>Blackboard</span></li>
+								<li><h3>Deliverable</h3><span>Speech outline and video link</span></li>
+								<li><h3>Description</h3><p>Please scan that room with the camera so that I can see your audience before you  begin spealing. You need to speak to a LIVE audience with 2-3  people for each speech. Sorry, animals are great supports but cannot critique your speech.
+									Submit your speech outline and video link to Blackboard > Submissions > Self-Introduction Speech. Details of this assignment are located in the previous module: Module 2 > Topics > Self-Introduction Speeches. 
+									For each speech, you will upload your video and PowerPoint presentations and then combine them using either Zentation or Knovio. For instructions on how to do this, please visit: Combining PowerPoint with Video. (This is located in Module 1 > Topics > Course Technology > Combining PowerPoint with Video.) Videos must be accessible via a link for classmates and the professor to readily click on. Individual videos submitted in .mp4, .wmv, etc. will not be accepted unless they are placed into a zentation, knovio, or a similar third party platform for viewing with PPT.
+									</p>
+								</li>
+							</ul>	
+						</li>
+						<li id="odu_assignment_2">
+							<ul class="odu_assignmentDetails">
+								<li><h2>1.A.2 Review Required Materials</h2></li>
+								<li><h3>Due</h3>May 31, 2001 at 11:59 PM</li>
+								<li><h3>Submit Via</h3>Blackboard</li>
+								<li><h3>Deliverable</h3>Speech outline and video link</li>
+								<li><p>Please scan that room with the camera so that I can see your audience before you  begin spealing. You need to speak to a LIVE audience with 2-3  people for each speech. Sorry, animals are great supports but cannot critique your speech.
+									Submit your speech outline and video link to Blackboard > Submissions > Self-Introduction Speech. Details of this assignment are located in the previous module: Module 2 > Topics > Self-Introduction Speeches. 
+									For each speech, you will upload your video and PowerPoint presentations and then combine them using either Zentation or Knovio. For instructions on how to do this, please visit: Combining PowerPoint with Video. (This is located in Module 1 > Topics > Course Technology > Combining PowerPoint with Video.) Videos must be accessible via a link for classmates and the professor to readily click on. Individual videos submitted in .mp4, .wmv, etc. will not be accepted unless they are placed into a zentation, knovio, or a similar third party platform for viewing with PPT.
+									</p>
+								</li>
+							</ul>	
+						</li>
+					</ul>
+				</div>
+			</section>
+
 	</body>
 </html>
