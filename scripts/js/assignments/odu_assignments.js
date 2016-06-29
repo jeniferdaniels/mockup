788833c0f25b3bd274f58011cbff4ae503319ea3
Jen Daniels <jdaniels@odu.edu>
2016-06-29 19:37:32 -0400

    created. partially functional. does not display all data correctly now. //TODO: fix display of data

---
+++ scripts/js/assignments/odu_assignments.js
@@ -0,0 +1,80 @@
+DEBUG = true;
+
+function writeAssignment(baseDiv, assignmentData){
+	if (DEBUG) benchMark("start", "writeAssignment", {"baseDiv": baseDiv, "assignmentData":assignmentData}); 
+
+		$(baseDiv)
+			.append($("<li>").attr("id","odu_assignment_" + assignmentData.id)
+				.append($("<ul>").attr("id","odu_assignmentDetails_" + assignmentData.id).addClass("odu_assignmentDetails")
+					.append($("<li>")
+						.append($("<h2>").html(assignmentData.title + " " + assignmentData.longTitle)))//end li
+					.append($("<li>")
+						.append($("<h3>").html("Due"))
+						.append($("<span>").html(assignmentData.dueDate))))); //TODO: format with moment
+
+				if (assignmentData.submitVia != ""){
+					$("#odu_assignmentDetails_" + assignmentData.id)
+						.append($("<li>")
+							.append($("<h3>").html("Submit Via"))
+							.append($("<span>").html(assignmentData.submitVia)));
+				}
+				
+				$("#odu_assignmentDetails_" + assignmentData.id)
+					.append($("<li>")
+						.append($("<h3>").html("Deliverable"))
+						.append($("<span>").html(assignmentData.deliverable))
+					.append($("<li>")
+						.append($("<h3>").html("Description"))
+						.append($("<p>").html(assignmentData.description))));//end li 1
+	
+	
+	if (DEBUG) benchMark("end", "writeAssignment", {"baseDiv": baseDiv, "assignmentData":assignmentData}); 
+
+}
+
+function writeAssignments(baseDiv, assignmentsData){
+
+		if (DEBUG) benchMark("start", "writeAssignments", {"baseDiv": baseDiv, "assignmentsData":assignmentsData}); 
+
+		//create base list
+		$(baseDiv).append($("<section>").attr("id","odu_assignmentsSection")
+			.append($("<div>").addClass("odu_assignmentsWrapper wrapper")
+				.append($("<ul>").attr("id","odu_assignments"))
+			)//end div;
+		)//end basediv
+	
+		//create list item for each assignment
+		for (i=0; i < assignmentsData.assignments.length; i++){
+			writeAssignment("#odu_assignments", assignmentsData.assignments[i]);
+		}
+	
+	if (DEBUG) benchMark("end", "writeAssignments", {"baseDiv": baseDiv, "assignmentData":assignmentsData}); 
+
+}
+
+
+function loadAssignments(baseDiv, id, listType){
+	if (DEBUG) benchMark("start", "loadAssignments", {"baseDiv": baseDiv, "id":id, "listType": listType }); 
+	
+	var assignmentsUrl = "sampleJson/sampleAssignmentsAll.json"; //default
+	if (listType == "assignment")
+		assignmentsUrl = "sampleJson/sampleAssignmentsSingular.json";
+	else if (listType == "module")
+		assignmentsUrl = "sampleJson/sampleAssigmentsModule.json";
+
+	//get data
+	$.ajax({
+		url: assignmentsUrl,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data) { console.log("got assignments data"); writeAssignments(baseDiv, data); },
+		error: function() { $(baseDiv).append("Unable to load assignment(s) right now."); console.log("There was an error loading the assignment(s)."); },
+		xhrFields: { withCredentials: true	},
+		crossDomain: true
+	});	
+	
+	if (DEBUG) benchMark("end", "loadAssignments", {"baseDiv": baseDiv, "id":id, "listType": listType }); 
+
+}
+
+
