cb033ea2d476d27b6a80f3f5f57ce4e28defb8aa
Jen Daniels <jdaniels@odu.edu>
2016-07-07 12:42:20 -0400

    updated li so description isnt under deliverable added error checking and documentation for expected keys

--- scripts/js/assignments/odu_assignments.js
+++ scripts/js/assignments/odu_assignments.js
@@ -1,38 +1,47 @@
 DEBUG = true;
 
-//TODO: JMD make this robust, currently there is not error/format checking.
+//used when writing assignments on assignments page, and used when writing assignments 
+//in the popup from the calendars
 //baseDiv does not include "#"
 function writeAssignment(baseDiv, assignmentData){
 	if (DEBUG) benchMark("start", "writeAssignment", {"baseDiv": baseDiv, "assignmentData":assignmentData}); 
 	
 	if (!isEmpty(baseDiv)){
-		if ($("#" + baseDiv).length){
-			$("#" + baseDiv)
-				.append($("<li>").attr("id","odu_assignment_" + assignmentData.id)
-					.append($("<ul>").attr("id","odu_assignmentDetails_" + assignmentData.id).addClass("odu_assignmentDetails")
-						.append($("<li>")
-							.append($("<i>").attr("id","odu_assignmentCheckmark_" + assignmentData.id).addClass("fa fa-check fa-3x displayHidden"))
-							.append($("<h2>").html(assignmentData.title + " " + assignmentData.longTitle)))//end li
-						.append($("<li>")
-							.append($("<h3>").html("Due-"))
-							.append($("<span>").html((moment(assignmentData.start).format('MMMM DD, YYYY h:mm A')))))));
+		if ($("#" + baseDiv).length){	
+			expectedKeys = ["id", "title", "longTitle", "start", "url", "deliverable", "description"];
+				//end optional.  included in api call calendar
+				//start = due date, submitVia is optional from all calls because of 7/7/2016 it isnt fully implemented
+				//it only gets filled if we know its a blackboard deliverable 
+				if (!hasEmptyKeys(assignmentData, expectedKeys)){	
+				
+					$("#" + baseDiv)
+						.append($("<li>").attr("id","odu_assignment_" + assignmentData.id)
+							.append($("<ul>").attr("id","odu_assignmentDetails_" + assignmentData.id).addClass("odu_assignmentDetails")
+								.append($("<li>")
+									.append($("<i>").attr("id","odu_assignmentCheckmark_" + assignmentData.id).addClass("fa fa-check fa-3x displayHidden"))
+									.append($("<h2>").html(assignmentData.title + " " + assignmentData.longTitle)))//end li
+								.append($("<li>")
+									.append($("<h3>").html("Due-"))
+									.append($("<span>").html((moment(assignmentData.start).format('MMMM DD, YYYY h:mm A')))))));
 
-					//optional data
-					if (!isEmpty(assignmentData.submitVia)){
-						$("#odu_assignmentDetails_" + assignmentData.id)
-							.append($("<li>")
-								.append($("<h3>").html("Submit Via-"))
-								.append($("<span>").html(assignmentData.submitVia)));
-					}
-					
-					
-					$("#odu_assignmentDetails_" + assignmentData.id)
-						.append($("<li>")
-							.append($("<h3>").html("Deliverable-"))
-							.append($("<span>").html(assignmentData.deliverable))
-						.append($("<li>")
-							.append($("<h3>").html("Description-"))
-							.append($("<p>").html(assignmentData.description))));//end li 1
+							//optional data
+							if (!isEmpty(assignmentData.submitVia)){
+								$("#odu_assignmentDetails_" + assignmentData.id)
+									.append($("<li>")
+										.append($("<h3>").html("Submit Via-"))
+										.append($("<span>").html(assignmentData.submitVia)));
+							}
+							
+							
+							$("#odu_assignmentDetails_" + assignmentData.id)
+								.append($("<li>")
+									.append($("<h3>").html("Deliverable-"))
+									.append($("<span>").html(assignmentData.deliverable)))
+								.append($("<li>")
+									.append($("<h3>").html("Description-"))
+									.append($("<p>").html(assignmentData.description)));//end li 1
+			}
+			else { if (DEBUG) console.log("one or more expected keys in assignment object missing");}
 		}
 		else { if (DEBUG) console.log("baseDiv " + baseDiv + " not found.");}
 	}
